a11be01855df45acb2ce4f273bab6cc6a8ee5c3a
MD Musa <md.musa@medsolv.com>
2021-06-07 13:56:25 +0200

    feat(wip): coming soon page

--- src/components/TwitterCommunity/TwitterProfile.tsx
+++ src/components/TwitterCommunity/TwitterProfile.tsx
@@ -3,9 +3,9 @@ import AvatarImg from "../../assets/avatar.webp";
 import useSWR from "swr";
 
 type TwitterProfileProps = {
-  thumbnailUrl: string;
-  url: string;
-  title: string;
+  name: string;
+  profile_image_url: string;
+  username: string;
 };
 const fetcher = (url: string) =>
   fetch(url, {
@@ -17,8 +17,11 @@ const fetcher = (url: string) =>
   }).then((r) => r.json());
 const TwitterProfile: React.FC<{}> = () => {
   const { data, error } = useSWR(
-    "https://jsonplaceholder.typicode.com/photos",
-    fetcher
+    "https://mnaus.api.stdlib.com/ultrasound-money@0.0.1/",
+    fetcher,
+    {
+      revalidateOnFocus: true,
+    }
   );
   if (error) return <div>failed to load</div>;
   if (!data) return <div>loading...</div>;
@@ -26,25 +29,25 @@ const TwitterProfile: React.FC<{}> = () => {
     <>
       <div className="flex flex-wrap">
         {data &&
-          data.slice(0, 50).map((item: TwitterProfileProps, index: number) => (
+          data.slice(0, 100).map((item: TwitterProfileProps, index: number) => (
             <div key={index} className="m-2 w-10 h-10">
               <a
                 target="_blank"
-                // href="https://twitter.com/"
-                href={item.url}
+                href="https://twitter.com/"
+                // href={item.url}
                 rel="noopener noreferrer"
                 role="link"
-                title={item.title}
+                title={item.name}
               >
                 <img
                   className="rounded-full w-full"
                   src={
-                    item.thumbnailUrl !== undefined
-                      ? item.thumbnailUrl
+                    item.profile_image_url !== undefined
+                      ? item.profile_image_url
                       : AvatarImg
                   }
-                  title={item.title}
-                  alt={item.title}
+                  title={item.name}
+                  alt={item.name}
                 />
               </a>
             </div>
--- src/components/TwitterCommunity/index.tsx
+++ src/components/TwitterCommunity/index.tsx
@@ -10,9 +10,10 @@ const TwitterCommunity: React.FC<TwitterCommunityPros> = ({ Data }) => {
       <h1 className="text-white text-2xl md:text-3xl leading-none text-center font-light mb-8">
         {Data.title_community}
       </h1>
-      <p className="text-white leading-6 md:leading-none text-center font-light text-base lg:text-lg mb-14">
-        {Data.description_community}
-      </p>
+      <p
+        className="text-white leading-6 md:leading-none text-center font-light text-base lg:text-lg mb-14"
+        dangerouslySetInnerHTML={{ __html: Data.description_community }}
+      />
       <TwitterProfile />
     </>
   );
