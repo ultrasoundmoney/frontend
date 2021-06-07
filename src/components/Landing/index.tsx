723e02e54ed61c08777284c08caaca114ae25ba6
MD Musa <md.musa@medsolv.com>
2021-06-07 13:44:07 +0200

    feat(dev): wip

---
+++ src/components/Landing/index.tsx
@@ -0,0 +1,26 @@
+import * as React from "react";
+import Navigation from "../Navigation";
+import Intro from "./Intro";
+
+const LandingPage: React.FC<{ Data?: Data }> = ({ Data }) => {
+  return (
+    <>
+      <div className="wrapper bg-blue-midnightexpress">
+        <div className="container m-auto tt">
+          <Navigation Data={Data} />
+          <Intro Data={Data} />
+          <div className="flex">
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam,
+              ducimus ipsam beatae veniam esse nisi perspiciatis iste accusamus
+              aliquid culpa. Voluptatem repellendus ut quibusdam sed pariatur
+              esse ipsam voluptates quod.
+            </p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default LandingPage;
