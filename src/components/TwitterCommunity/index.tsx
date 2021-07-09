89e82870a34d48a78b8280edb37248ef75116848
Alexander Tesfamichael <alex.tesfamichael@gmail.com>
2021-07-09 02:41:36 +0200

    Fix and restyle copy batsound
    Fixes copying of batsound on iOS.
    Restyles with new design to make copyability more obvious.

--- src/components/TwitterCommunity/index.tsx
+++ src/components/TwitterCommunity/index.tsx
@@ -1,8 +1,10 @@
 import * as React from "react";
 import useSWR from "swr";
 import twemoji from "twemoji";
+import Clipboard from "react-clipboard.js";
 import { useTranslations } from "../../utils/use-translation";
 import TwitterProfile from "./TwitterProfile";
+import { default as clipboardImage } from "../../assets/clipboard.png";
 
 type TwitterCommunityPros = {};
 const fetcher = (url: string) =>
@@ -16,8 +18,9 @@ const fetcher = (url: string) =>
 
 const TwitterCommunity: React.FC<TwitterCommunityPros> = () => {
   const { translations: t } = useTranslations();
-  const [isCopied, setIsCopied] = React.useState<boolean>(false);
-  const [tool, setTool] = React.useState(false);
+  const [isCopiedFeedbackVisible, setIsCopiedFeedbackVisible] = React.useState<
+    boolean
+  >(false);
   const { data } = useSWR(
     "https://api.ultrasound.money/fam/2/profiles",
     fetcher,
@@ -26,39 +29,27 @@ const TwitterCommunity: React.FC<TwitterCommunityPros> = () => {
       revalidateOnReconnect: false,
     }
   );
+
   if (!data) {
     return null;
   }
+
   const getFamCount = new Intl.NumberFormat().format(
     isNaN(data && data.count) ? 0 : data && data.count
   );
+
   const getText =
     getFamCount !== undefined
       ? t.title_community.replace("#XXX", getFamCount)
       : t.title_community;
-  const handelClickedToCopyText = () => {
-    if (navigator.clipboard) {
-      navigator.clipboard
-        .writeText("🦇🔊")
-        .then(() => {
-          setIsCopied(true);
-        })
-        .catch(() => setIsCopied(false));
-    } else {
-      setIsCopied(false);
-    }
+
+  const onBatSoundCopied = () => {
+    setIsCopiedFeedbackVisible(true);
+    setTimeout(() => setIsCopiedFeedbackVisible(false), 800);
   };
-  const handleMouseEnter = () => {
-    const parent = document.getElementById("bat-sound");
-    parent.onmouseenter = () => {
-      setTool(true);
-    };
-    parent.onmouseleave = () => {
-      setTool(false);
-    };
-  };
+
   return (
-    <>
+    <div className="flex flex-col align-center">
       <h1 className="text-white text-2xl md:text-3xl text-center font-light mb-8">
         <a
           target="_blank"
@@ -71,21 +62,39 @@ const TwitterCommunity: React.FC<TwitterCommunityPros> = () => {
           {getText}
         </a>
       </h1>
-      <div className={`clipboard ${tool ? "block" : "hidden"}`}>
-        {isCopied ? "copied" : "click to copy"}
+      {/* Getting these elements to line up was extremely fiddely, beware of changing any padding. Would be great to improve this with absolute asset sizes and padding sizes */}
+      <div className="flex justify-center flex-col md:flex-row text-center mb-14">
+        <p className="my-2 text-white leading-6 md:leading-none font-light text-base lg:text-lg">
+          {t.description_community_1}
+        </p>
+        <Clipboard
+          className="md:mx-4 flex justify-center"
+          data-clipboard-text={"🦇🔊"}
+          onSuccess={onBatSoundCopied}
+        >
+          <span
+            className={`absolute px-5 pt-1 pb-2 rounded-full border border-gray-700 transition duration-500 ease-in-out ${
+              isCopiedFeedbackVisible
+                ? "bg-gray-700 text-white"
+                : "bg-transparent text-transparent"
+            }`}
+          >
+            copied!
+          </span>
+          <span
+            className="border border-gray-700 pl-3 pt-1 pb-2 px-12 pr-9 rounded-full batsound-emoji"
+            dangerouslySetInnerHTML={{ __html: twemoji.parse("🦇🔊") }}
+          />
+          <div className="border border-gray-700 -ml-8 pl-1 pt-2 pb-2 rounded-r-full">
+            <img className="clipboard-image" src={clipboardImage} />
+          </div>
+        </Clipboard>
+        <p className="my-2 text-white leading-6 md:leading-none font-light text-base lg:text-lg">
+          {t.description_community_2}
+        </p>
       </div>
-      <p
-        className={`text-white leading-6 md:leading-none text-center font-light text-base lg:text-lg mb-14 ${
-          isCopied ? "span-bat-sound-copied" : "span-bat-sound"
-        }`}
-        dangerouslySetInnerHTML={{
-          __html: twemoji.parse(t.description_community),
-        }}
-        onClick={handelClickedToCopyText}
-        onMouseEnter={handleMouseEnter}
-      />
       <TwitterProfile profileList={data && data.profiles} />
-    </>
+    </div>
   );
 };
 
