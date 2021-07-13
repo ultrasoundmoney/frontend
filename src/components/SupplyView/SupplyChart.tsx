196346784b6d3bf709cdc79f655becb0597faf56
Alexander Tesfamichael <alex.tesfamichael@gmail.com>
2021-07-13 18:47:45 +0200

    Consistent casing for historical marker labels

--- src/components/SupplyView/SupplyChart.tsx
+++ src/components/SupplyView/SupplyChart.tsx
@@ -127,10 +127,10 @@ const SupplyChart: React.FC<Props> = ({
     // prettier-ignore
     (): [DateTime | null, string, string][] => ([
       [DateTime.fromISO("2015-07-31T00:00:00Z"), t.marker_genesis, `5 ETH/${t.marker_block}`],
-      [DateTime.fromISO("2017-10-16T00:00:00Z"), "Byzantium", `3 ETH/${t.marker_block}`],
-      [DateTime.fromISO("2019-02-27T00:00:00Z"), "Constantinople", `2 ETH/${t.marker_block}`],
+      [DateTime.fromISO("2017-10-16T00:00:00Z"), "byzantium", `3 ETH/${t.marker_block}`],
+      [DateTime.fromISO("2019-02-27T00:00:00Z"), "constantinople", `2 ETH/${t.marker_block}`],
       [DateTime.fromISO("2020-12-01T00:00:00Z"), t.marker_phase_0, t.marker_pos],
-      [LONDON_DATE, "London", "EIP1559"],
+      [LONDON_DATE, "london", "EIP1559"],
       [chartSettings.projectedMergeDate, t.marker_merge, t.marker_pow_removal],
     ]),
     [chartSettings, t]
