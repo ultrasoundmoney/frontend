53ee6025aa55052d2110d3b451ba9b97ae4bf491
Alexander Tesfamichael <alex.tesfamichael@gmail.com>
2021-07-13 14:50:10 +0200

    Add comment for important field in next config

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@ const withImages = require("next-images");
 module.exports = withImages({
   future: { webpack5: true },
   trailingSlash: true,
+  // Without the exportPathMap fleek deployments for /page break.
   exportPathMap: () => ({
     "/": { page: "/" },
     "/landing": { page: "/landing" },
