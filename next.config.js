e8e27b20d02c920023051878e01188f1d58c0b73
Alexander Tesfamichael <alex.tesfamichael@gmail.com>
2021-07-13 14:35:21 +0200

    Fix next.config.js
    Half the config wasn't used due to overwriting exports.
    Remove the PWA.

--- next.config.js
+++ next.config.js
@@ -1,24 +1,9 @@
-module.exports = {
-  future: {
-    webpack5: true,
-  },
-  exportTrailingSlash: true,
-  exportPathMap: function () {
-    return {
-      "/": { page: "/" },
-      "/landing": { page: "/landing" },
-    };
-  },
-};
-const withPWA = require("next-pwa");
-module.exports = withPWA({
-  pwa: {
-    dest: "public",
-  },
-});
 const withImages = require("next-images");
 module.exports = withImages({
-  webpack(config, options) {
-    return config;
-  },
+  future: { webpack5: true },
+  trailingSlash: true,
+  exportPathMap: () => ({
+    "/": { page: "/" },
+    "/landing": { page: "/landing" },
+  }),
 });
