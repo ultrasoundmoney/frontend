122ae970196be9dbe18955d5d4ddffaa630800ad
Alexander Tesfamichael <alex.tesfamichael@gmail.com>
2021-07-14 13:21:01 +0200

    Consistent margin between coming soon elements

--- src/components/ComingSoon/index.tsx
+++ src/components/ComingSoon/index.tsx
@@ -52,30 +52,30 @@ const ComingSoon: React.FC = () => {
               <CountDown targetDate="August 4, 2021" targetTime="15:00:00" />
             </div>
           </div>
-          <div className="flex">
+          <div className="flex mb-16">
             <div className="w-full md:w-5/6 lg:w-3/6 md:m-auto px-4 md:px-0">
               <EthBlocks
                 currentBlockNr={currentBlockNumber && currentBlockNumber}
               />
             </div>
           </div>
-          <div className="flex flex-col px-4 md:px-0 pt-8 md:pt-40">
-            <h1 className="text-white font-extralight text-xl md:text-3xl xl:text-41xl text-center mb-8">
+          <div className="flex flex-col px-4 md:px-0 md:pt-40 mb-16">
+            <h1 className="text-white font-extralight text-2xl md:text-3xl xl:text-41xl text-center mb-4">
               supply graph
             </h1>
-            <p className="text-white text-center font-light text-base lg:text-lg mb-16">
+            <p className="text-white text-center font-light text-base lg:text-lg mb-8">
               ready to discover the future of ETH?
             </p>
-            <div className="w-full md:w-5/6 lg:w-5/6 md:m-auto relative bg-blue-tangaroa py-8 md:px-16 mb-16">
+            <div className="w-full md:w-5/6 lg:w-5/6 md:m-auto relative bg-blue-tangaroa md:px-16">
               <SupplyView />
             </div>
           </div>
-          <div className="flex px-4 md:px-0 pt-8 md:pt-40">
+          <div className="flex px-4 md:px-0 pt-8 md:pt-40 mb-16">
             <div className="w-full md:w-5/6 lg:w-2/3 md:m-auto relative">
               <TwitterCommunity />
             </div>
           </div>
-          <div className="flex px-4 md:px-0 py-24">
+          <div className="flex px-4 md:px-0">
             <div className="w-full md:w-5/6 lg:w-2/3 md:m-auto relative">
               <FollowingYou />
             </div>
