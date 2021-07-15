7d5f217b6352bcecf86aedfa51ca330428953ed0
Alexander Tesfamichael <alex.tesfamichael@gmail.com>
2021-07-15 13:14:49 +0200

    Lower performance sample rate in production

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,10 +8,7 @@ Sentry.init({
     "https://3ef07f75a826463a94510b8d676fd7e3@o920717.ingest.sentry.io/5866579",
   integrations: [new Integrations.BrowserTracing()],
 
-  // Set tracesSampleRate to 1.0 to capture 100%
-  // of transactions for performance monitoring.
-  // We recommend adjusting this value in production
-  tracesSampleRate: 1.0,
+  tracesSampleRate: process.env.ENV === "dev" ? 1 : 0.2,
 });
 
 export default function MyApp({ Component, pageProps }: AppProps) {
