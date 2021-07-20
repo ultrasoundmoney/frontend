a5bbeaa13f7da7e6e1976909481ea62bc1704d55
Alexander Tesfamichael <alex.tesfamichael@gmail.com>
2021-07-20 13:35:45 +0200

    Formatting

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,71 +1,68 @@
-const { colors } = require('tailwindcss/defaultTheme')
+const { colors } = require("tailwindcss/defaultTheme");
 module.exports = {
   // mode: 'jit',
   darkMood: "media",
   purge: [
     "./public/**/*.html",
-    "./src/pages/**/*.{js,ts,jsx,tsx}", 
-    "./src/components/**/*.{js,ts,jsx,tsx}"
+    "./src/pages/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
       colors: {
-        black:{
+        black: {
           ...colors.black,
         },
         gray: {
           ...colors.gray,
         },
-        green:{
+        green: {
           ...colors.green,
-          'mediumspring': '#00FFA3',
+          mediumspring: "#00FFA3",
         },
-        blue:{
+        blue: {
           ...colors.blue,
-          midnightexpress: '#131827', 
-          tangaroa: '#1b2236',
-          spindle: '#b5bddb',
-          linkwater: '#c8cbd9',
-          shipcove: "#8991ad"
+          midnightexpress: "#131827",
+          tangaroa: "#1b2236",
+          spindle: "#b5bddb",
+          linkwater: "#c8cbd9",
+          shipcove: "#8991ad",
         },
       },
-      width: {
-
-      },
-      spacing:{
-        '42': '165px',
+      width: {},
+      spacing: {
+        "42": "165px",
       },
       lineHeight: {
-        '2': '2',
-        'card': '3.3',
-        '18': '1.18',
-        'loose1': '2.95',
+        "2": "2",
+        card: "3.3",
+        "18": "1.18",
+        loose1: "2.95",
       },
       fontFamily: {
-        'roboto' : ['Roboto Mono', 'monospace'],
+        roboto: ["Roboto Mono", "monospace"],
       },
       transitionTimingFunction: {
-        'ease-out-quint': 'cubic-bezier(0.23, 1, 0.32, 1)'
+        "ease-out-quint": "cubic-bezier(0.23, 1, 0.32, 1)",
       },
       boxShadow: {
-        outline: '0 0 0 1px rgba(229, 103, 86, 0.4)',
+        outline: "0 0 0 1px rgba(229, 103, 86, 0.4)",
       },
-      fontSize:{
-        '41xl': '2.5rem',
-        '21xl': '1.87rem'
-      }
-    }
+      fontSize: {
+        "41xl": "2.5rem",
+        "21xl": "1.87rem",
+      },
+    },
   },
-  variants: {
-  },
+  variants: {},
   plugins: [
-		require('tailwindcss'),
-		require('precss'),
-		require('autoprefixer'),
-    require('postcss-import'),
-	],
+    require("tailwindcss"),
+    require("precss"),
+    require("autoprefixer"),
+    require("postcss-import"),
+  ],
   future: {
     removeDeprecatedGapUtilities: true,
-		purgeLayersByDefault: true,
+    purgeLayersByDefault: true,
   },
-}
+};
