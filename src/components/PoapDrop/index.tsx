3da7f01e0997d5d49240a4746a7b00672f8b513a
Justin Drake <drakefjustin@gmail.com>
2021-07-20 21:47:11 +0100

    rework ultra sound POAP drop

--- src/components/PoapDrop/index.tsx
+++ src/components/PoapDrop/index.tsx
@@ -36,7 +36,7 @@ const PoapDrop: React.FC = () => {
         style={{
           backgroundImage: isMd ? `url(${bgMountains})` : "none",
         }}
-        className="rounded-xl relative bg-blue-tangaroa md:bg-cover md:border md:border-gray-500 md:px-8 md:py-12 lg:px-16 lg:py-20 xl:px-24 xl:py-24 2xl:text-xl 2xl:px-32"
+        className="rounded-xl relative bg-blue-tangaroa md:bg-cover md:px-8 md:py-12 lg:px-16 lg:py-20 xl:px-24 xl:py-24 2xl:text-xl 2xl:px-32"
       >
         <div className="flex items-center">
           <SpanMoji
@@ -48,60 +48,69 @@ const PoapDrop: React.FC = () => {
             emoji="🔊"
           />
           <img
-            className="w-16 md:w-12 absolute left-4 top-4 md:static md:inline lg:w-16"
+            className="w-16 md:w-12 lg:w-16 absolute left-32 top-24 md:static md:inline invisible md:visible"
             src={poapLogoGray}
           />
         </div>
-        <div className="flex flex-col md:absolute md:right-10 md:-top-8 lg:right-24 xl:right-20 xl:-top-16">
-          <video
-            className="w-full rounded-xl md:w-64 lg:w-72 xl:w-96"
-            src="/poap-drop.mp4"
-            playsInline
-            autoPlay
-            muted
-            loop
-            poster={poapDropPoster}
-            ref={refVideo}
-          >
-            <source src="/public/poap-drop.mp4" type="video/mp4" />
-            <source src="/public/poap-drop.webm" type="video/webm" />
-            <source src="/public/poap-drop.ogv" type="video/ogg" />
-          </video>
-          <button
-            className="bg-blue-tangaroa text-xl rounded-full text-white py-2 px-2 bottom-8 mr-4 ml-auto -mt-14 z-10 hover:opacity-90 opacity-70"
+        <div className="flex flex-col md:absolute md:right-10 md:-top-8 lg:right-24 xl:right-20 xl:-top-16 items-center">
+          <div
             onClick={handleOnToggleMute}
+            className="w-96 md:w-64 lg:w-72 xl:w-96 mt-20 md:mt-0 cursor-pointer"
           >
-            {muted ? <SpanMoji emoji="🔇" /> : <SpanMoji emoji="🔊" />}
-          </button>
+            <img
+              className="relative w-16 left-4 top-24 -mt-20 background md:invisible"
+              src={poapLogoGray}
+            />
+            <video
+              src="/poap-drop.mp4"
+              className="rounded-xl"
+              playsInline
+              autoPlay
+              muted
+              loop
+              poster={poapDropPoster}
+              ref={refVideo}
+            >
+              <source src="/public/poap-drop.mp4" type="video/mp4" />
+              <source src="/public/poap-drop.webm" type="video/webm" />
+              <source src="/public/poap-drop.ogv" type="video/ogg" />
+            </video>
+            <button className="bg-blue-tangaroa text-xl rounded-full text-white py-3 px-3 hover:opacity-90 opacity-75 z-10 float-right mr-4 -mt-16">
+              {muted ? <SpanMoji emoji="🔇" /> : <SpanMoji emoji="🔊" />}
+            </button>
+          </div>
         </div>
-        <div className="px-8 py-8 md:p-0">
-          <h2 className="text-white text-2xl md:text-3xl font-light my-8">
-            {t.title_poap_drop}
-          </h2>
+        <div className="px-8 py-8 md:p-0 mt-8">
           <ul className="list-disc list-inside leading-8">
-            <li className="text-white">
+            <li className="text-white whitespace-pre">
               <span className="font-bold">
                 {t.description_poap_drop_1_left}
               </span>
               {t.description_poap_drop_1_right}
             </li>
-            <li className="text-white">
+            <li className="text-white whitespace-pre">
               <span className="font-bold">
                 {t.description_poap_drop_2_left}
               </span>
               {t.description_poap_drop_2_right}
             </li>
-            <li className="text-white">
+            <li className="text-white whitespace-pre">
               <span className="font-bold">
                 {t.description_poap_drop_3_left}
               </span>
               {t.description_poap_drop_3_right}
             </li>
-            <li className="text-white">
+            <li className="text-white whitespace-pre">
               <span className="font-bold">
                 {t.description_poap_drop_4_left}
               </span>
               {t.description_poap_drop_4_right}
+            </li>
+            <li className="text-white whitespace-pre">
+              <span className="font-bold">
+                {t.description_poap_drop_5_left}
+              </span>
+              {t.description_poap_drop_5_right}
             </li>
           </ul>
         </div>
