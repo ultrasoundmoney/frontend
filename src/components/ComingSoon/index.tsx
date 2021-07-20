fa7768301c26d16f86b24dc9f44c0b1bc672a1dd
Alexander Tesfamichael <alex.tesfamichael@gmail.com>
2021-07-20 10:02:40 +0200

    Remove redundant width class

--- src/components/ComingSoon/index.tsx
+++ src/components/ComingSoon/index.tsx
@@ -66,7 +66,7 @@ const ComingSoon: React.FC = () => {
           <p className="text-white text-center font-light text-base lg:text-lg mb-8">
             {t.teaser_supply_subtitle}
           </p>
-          <div className="w-full lg:w-5/6 md:m-auto relative bg-blue-tangaroa px-2 md:px-4 xl:px-12 py-4 md:py-8 xl:py-12 rounded-xl">
+          <div className="w-full md:w-5/6 md:m-auto relative bg-blue-tangaroa md:px-8 py-4 md:py-8 md:py-16 rounded-xl">
             <SupplyView />
           </div>
         </div>
