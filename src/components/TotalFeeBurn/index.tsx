6fa808deee1a70359bb1afd69ab4e71dce8fe5fc
Alexander Tesfamichael <alex.tesfamichael@gmail.com>
2021-07-30 01:51:27 +0200

    make burn update more obvious

--- src/components/TotalFeeBurn/index.tsx
+++ src/components/TotalFeeBurn/index.tsx
@@ -4,13 +4,15 @@ import SpanMoji from "../SpanMoji";
 import CountUp from "react-countup";
 
 const weiToEth = (wei: number): number => wei / 10 ** 18;
+const weiToGwei = (wei: number): number => wei / 10 ** 9;
 const socketUrl = "ws://api.ultrasound.money/fees-ropsten/base-fee-feed";
 
 const TotalFeeBurn: FC = () => {
   const { lastJsonMessage } = useWebSocket(socketUrl, { share: true });
   const totalFeesBurned = lastJsonMessage?.totalFeesBurned;
+  const blockNr = lastJsonMessage?.number;
 
-  const prevTotalFeesBurnedRef = useRef(0);
+  const prevTotalFeesBurnedRef = useRef(undefined);
   useEffect(() => {
     if (typeof totalFeesBurned === "number")
       prevTotalFeesBurnedRef.current = totalFeesBurned;
@@ -19,31 +21,34 @@ const TotalFeeBurn: FC = () => {
 
   return (
     <div className="bg-blue-tangaroa w-full rounded-lg p-8 md:p-16 md:w-2/3 md:mx-auto">
-      <p className="text-blue-spindle mb-4 md:mb-8 md:text-xl">
-        Fees Burned{" "}
-        <span className="animate-pulse">
-          <SpanMoji emoji="🔥" />
-        </span>
-      </p>
+      <div className="flex justify-between">
+        <p className="text-blue-spindle mb-4 md:mb-8 md:text-xl">
+          Fees Burned{" "}
+          <span className="animate-pulse">
+            <SpanMoji emoji="🔥" />
+          </span>
+        </p>
+      </div>
 
       {typeof totalFeesBurned === "number" ? (
-        <CountUp
-          start={weiToEth(prevTotalFeesBurned)}
-          end={weiToEth(totalFeesBurned)}
-          duration={5}
-          decimals={4}
-          suffix=" ETH"
-        >
-          {({ countUpRef }) => (
-            <p
-              className="text-white text-3xl md:text-4xl lg:text-5xl"
-              ref={countUpRef}
-            ></p>
-          )}
-        </CountUp>
+        <p className="text-white text-3xl md:text-4xl lg:text-5xl">
+          <CountUp
+            decimals={14}
+            duration={4}
+            end={weiToEth(totalFeesBurned)}
+            suffix=" ETH"
+            preserveValue={true}
+          />
+        </p>
       ) : (
         <h1 className="text-white text-3xl md:text-4xl">loading...</h1>
       )}
+      <p className="text-blue-spindle mt-8 md:text-xl">
+        block {blockNr}
+        {typeof totalFeesBurned === "number" &&
+          typeof prevTotalFeesBurned === "number" &&
+          `, added ${weiToGwei(totalFeesBurned - prevTotalFeesBurned)} Gwei`}
+      </p>
     </div>
   );
 };
