cc4178b93a1276184dc4acd3fc52fb2cec330cd7
Alexander Tesfamichael <alex.tesfamichael@gmail.com>
2021-08-03 18:20:09 +0200

    add animate on enter

--- src/components/LatestBlocks/index.tsx
+++ src/components/LatestBlocks/index.tsx
@@ -1,14 +1,13 @@
-import React, { memo, FC, useRef, useEffect } from "react";
+import React, { memo, FC, useRef, useEffect, useMemo } from "react";
 import useWebSocket from "react-use-websocket";
 import { CSSTransition } from "react-transition-group";
+import _ from "lodash";
 
 const weiToGwei = (wei: number): number => wei / 10 ** 9;
 const formatter = Intl.NumberFormat("en", {
   minimumFractionDigits: 4,
 });
 const formatFee = (fee: number) => formatter.format(weiToGwei(fee));
-
-type FeeBlock = { number: number; fees: number };
 
 const LatestBlocks: FC = () => {
   const { lastJsonMessage } = useWebSocket(
@@ -18,58 +17,44 @@ const LatestBlocks: FC = () => {
       filter: (message) => JSON.parse(message.data).type === "base-fee-update",
     }
   );
-  const latestBlocks = useRef<FeeBlock[]>([]);
+  const messageHistory = useRef([]);
 
-  const lastBlockUpdateNumber = lastJsonMessage?.number;
-  const lastBlockUpdateFees = lastJsonMessage?.fees;
+  messageHistory.current = useMemo(
+    () => messageHistory.current.concat(lastJsonMessage),
+    [lastJsonMessage]
+  );
 
-  useEffect(() => {
-    if (
-      typeof lastBlockUpdateFees !== "number" ||
-      typeof lastBlockUpdateNumber !== "number"
-    ) {
-      return;
-    }
-
-    latestBlocks.current.push({
-      number: lastBlockUpdateNumber,
-      fees: lastBlockUpdateFees,
-    });
-
-    if (latestBlocks.current.length > 5) {
-      latestBlocks.current.shift();
-    }
-  }, [lastBlockUpdateNumber, lastBlockUpdateFees]);
-
-  console.log(latestBlocks);
-
-  const Block: FC<FeeBlock> = ({ number, fees }) => (
-    <li className="flex justify-between my-8">
-      <p className="text-white">Block {number}</p>
-      <p className="text-white">
-        {formatFee(fees)} <span className="text-blue-spindle">Gwei</span>
-      </p>
-    </li>
+  const latestBlocks = _.takeRight(
+    messageHistory.current.filter((msg) => msg !== null),
+    5
   );
 
   return (
     <div className="bg-blue-tangaroa w-full rounded-lg p-8 md:p-16 md:w-2/3 md:mx-auto">
-      <p className="font-inter font-light text-white mb-8 md:mb-8 md:text-xl">
+      <p className="font-inter font-light text-white mb-8 md:mb-8">
         latest blocks
       </p>
       <ul>
-        {latestBlocks.current.length === 0 ? (
+        {messageHistory.current.length === 0 ? (
           <p className="text-white md:text-4xl">loading...</p>
         ) : (
-          latestBlocks.current.map(({ number, fees }) => (
+          latestBlocks.map(({ number, fees }) => (
             <CSSTransition
-              in={true}
-              classNames="enter-fee-block"
+              in={latestBlocks.some((block) => block.number === number)}
+              classNames="fee-block"
               timeout={500}
               appear={true}
               key={number}
+              unmountOnExit={true}
+              enter={false}
             >
-              <Block number={number} fees={fees} />
+              <li className="flex justify-between my-8 fee-block">
+                <p className="text-white">Block {number}</p>
+                <p className="text-white">
+                  {formatFee(fees)}{" "}
+                  <span className="text-blue-spindle">Gwei</span>
+                </p>
+              </li>
             </CSSTransition>
           ))
         )}
