06d5f5dd28ed32fb73f0344edd325bf7d77cc604
Justin Drake <drakefjustin@gmail.com>
2021-08-04 23:08:26 +0200

    minor polish

--- src/components/ComingSoon/index.tsx
+++ src/components/ComingSoon/index.tsx
@@ -80,11 +80,11 @@ const ComingSoon: React.FC = () => {
               <LatestBlocks />
             </div>
           </div>
-          <div className="mb-16 lg:w-1/2 lg:pt-20 lg:pl-4">
+          <div className="lg:w-1/2 lg:pt-20 lg:pl-4">
             <BurnLeaderboard />
           </div>
         </div>
-        <div className="flex flex-col px-4 md:px-2 md:pt-40 mb-16">
+        <div className="flex flex-col px-4 md:px-2 pt-40 mb-16">
           <h1 className="text-white font-light text-center text-2xl md:text-3xl xl:text-41xl mb-8">
             {t.teaser_supply_title}
           </h1>
@@ -94,7 +94,7 @@ const ComingSoon: React.FC = () => {
           <div className="w-full lg:w-5/6 md:m-auto relative bg-blue-tangaroa px-2 md:px-4 xl:px-12 py-4 md:py-8 xl:py-12 rounded-xl">
             <SupplyView />
           </div>
-          <div className="flex px-4 md:px-0 pt-8 md:pt-40 mb-16">
+          <div className="flex px-4 md:px-0 pt-8 pt-40 mb-16">
             <div className="w-full md:w-5/6 lg:w-2/3 md:m-auto relative">
               <TwitterCommunity />
             </div>
