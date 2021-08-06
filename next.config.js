80724475098f48bcf2e7569f434f26515c7a9efa
Alexander Tesfamichael <alex.tesfamichael@gmail.com>
2021-08-06 15:32:33 +0200

    upgrade nextjs

--- next.config.js
+++ next.config.js
@@ -1,6 +1,5 @@
 const withImages = require("next-images");
 module.exports = withImages({
-  future: { webpack5: true },
   trailingSlash: true,
   // Without the exportPathMap fleek deployments for /page break.
   exportPathMap: () => ({
