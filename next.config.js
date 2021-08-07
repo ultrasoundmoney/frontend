ec6a71aaf0d1484007b438b135f51ba516194fe8
Alexander Tesfamichael <alex.tesfamichael@gmail.com>
2021-08-07 16:12:36 +0200

    downgrade next
    Needs wider changes, lets merge landing page first.

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 const withImages = require("next-images");
 module.exports = withImages({
+  future: { webpack5: true },
   trailingSlash: true,
   // Without the exportPathMap fleek deployments for /page break.
   exportPathMap: () => ({
