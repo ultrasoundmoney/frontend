672f8a508469e49fc4ee68b5419779bb40656f49
Alexander Tesfamichael <alex.tesfamichael@gmail.com>
2021-08-07 16:30:36 +0200

    add next bundle analyzer

--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
+const { flow } = require("lodash");
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === "true",
+});
 const withImages = require("next-images");
-module.exports = withImages({
+module.exports = flow([withBundleAnalyzer, withImages])({
   future: { webpack5: true },
   trailingSlash: true,
   // Without the exportPathMap fleek deployments for /page break.
