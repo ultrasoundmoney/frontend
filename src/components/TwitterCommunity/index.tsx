03af4bf3047e4e7f23bbc7f5cabcd335880b00b7
Alexander Tesfamichael <alex.tesfamichael@gmail.com>
2021-08-08 01:33:00 +0200

    fix and improve copy feedback

--- src/components/TwitterCommunity/index.tsx
+++ src/components/TwitterCommunity/index.tsx
@@ -62,14 +62,19 @@ const TwitterCommunity: React.FC = () => {
         <p className="text-blue-shipcove md:text-lg">wear the bat signal</p>
         <div className="w-4"></div>
         <Clipboard data-clipboard-text={"🦇🔊"} onSuccess={onBatSoundCopied}>
-          <span
-            className={`bg-blue-midnightexpress border border-gray-700 rounded-full p-2 pl-5 flex w-48 mx-auto justify-between items-center text-2xl transition duration-500 ease-in-out isolate clipboard-emoji ${
-              isCopiedFeedbackVisible ? "bg-gray-800" : ""
-            }`}
-          >
+          <span className="relative bg-blue-midnightexpress border border-gray-700 rounded-full p-2 pl-5 flex w-48 mx-auto justify-between items-center text-2xl isolate clipboard-emoji">
             <SpanMoji emoji="🦇🔊" />
             <span className="font-light text-base copy-container rounded-full bg-green-mediumspring text-blue-midnightexpress px-5 py-1 isolate">
               copy
+            </span>
+            <span
+              className={`absolute left-0 right-0 top-0 bottom-0 p-1 bg-blue-midnightexpress flex justify-center items-center rounded-full transition-opacity ${
+                isCopiedFeedbackVisible ? "opacity-100" : "opacity-0"
+              }`}
+            >
+              <p className="font-inter font-light text-base text-white">
+                copied!
+              </p>
             </span>
           </span>
         </Clipboard>
