f6ae0b404b1c45fe1b158f4a8ea1a87ccc231de9
Alexander Tesfamichael <alex.tesfamichael@gmail.com>
2021-08-25 07:47:52 +0200

    switch back to old api URL
    new cluster, old URL.

--- src/api.ts
+++ src/api.ts
@@ -7,7 +7,7 @@ export const famBasePath = "https://api-prod.ultrasound.money/fam";
 export const feesBasePath =
   config.apiEnv === "staging"
     ? "https://api-stag.ultrasound.money/fees"
-    : "https://api-prod.ultrasound.money/fees";
+    : "https://api.ultrasound.money/fees";
 
 type WeiPerMinute = number;
 type Wei = number;
