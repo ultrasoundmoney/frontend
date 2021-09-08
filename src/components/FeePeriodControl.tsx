687daf6e89ade4160fa1d177e16843197367dbb2
Alexander Tesfamichael <alex.tesfamichael@gmail.com>
2021-09-08 20:13:10 +0200

    remove unused code

--- src/components/FeePeriodControl.tsx
+++ src/components/FeePeriodControl.tsx
@@ -31,38 +31,6 @@ const FeePeriodControl: FC<FeePeriodControlProps> = ({
           {timeframe}
         </button>
       ))}
-      {/* <button */}
-      {/*   className={`font-inter text-sm px-4 py-1 border border-transparent ${ */}
-      {/*     timeframe === "t24h" ? activePeriodClasses : "text-blue-manatee" */}
-      {/*   }`} */}
-      {/*   onClick={() => onSetFeePeriod("t24h")} */}
-      {/* > */}
-      {/*   24h */}
-      {/* </button> */}
-      {/* <button */}
-      {/*   className={`font-inter text-sm px-4 py-1 border border-transparent ${ */}
-      {/*     timeframe === "t7d" ? activePeriodClasses : "text-blue-manatee" */}
-      {/*   }`} */}
-      {/*   onClick={() => onSetFeePeriod("t7d")} */}
-      {/* > */}
-      {/*   7d */}
-      {/* </button> */}
-      {/* <button */}
-      {/*   className={`font-inter text-sm px-4 py-1 border border-transparent ${ */}
-      {/*     timeframe === "t30d" ? activePeriodClasses : "text-blue-manatee" */}
-      {/*   }`} */}
-      {/*   onClick={() => onSetFeePeriod("t30d")} */}
-      {/* > */}
-      {/*   30d */}
-      {/* </button> */}
-      {/* <button */}
-      {/*   className={`font-inter text-sm px-4 py-1 border border-transparent ${ */}
-      {/*     timeframe === "tAll" ? activePeriodClasses : "text-blue-manatee" */}
-      {/*   }`} */}
-      {/*   onClick={() => onSetFeePeriod("tAll")} */}
-      {/* > */}
-      {/*   all */}
-      {/* </button> */}
     </div>
   );
 };
