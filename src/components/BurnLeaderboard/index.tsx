01c99f1e9838b23bc167042365c1b3493b1bd7ed
Justin Drake <drakefjustin@gmail.com>
2021-10-12 20:05:18 +0100

    minor polish to category tags

--- src/components/BurnLeaderboard/index.tsx
+++ src/components/BurnLeaderboard/index.tsx
@@ -111,7 +111,7 @@ const LeaderboardRow: FC<LeaderboardRowProps> = ({
             )}
           </p>
           {category && (
-            <p className="px-1 py-0 ml-2 text-sm rounded-sm font-extralight text-blue-shipcove hidden md:block lg:hidden xl:block bg-blue-highlightbg">
+            <p className="px-1.5 py-0.5 ml-2 text-sm rounded-sm text-blue-manatee font-normal hidden md:block lg:hidden xl:block bg-blue-highlightbg">
               {category}
             </p>
           )}
