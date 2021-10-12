e44acfb3c4f8ea8f2a847911554471b7330bce49
Justin Drake <drakefjustin@gmail.com>
2021-10-12 12:51:05 +0100

    update supply and issuance data

--- src/static-ether-data.ts
+++ src/static-ether-data.ts
@@ -1,3 +1,3 @@
 export const powIssuancePerDay = 13500; // see https://etherscan.io/chart/blockreward
-export const posIssuancePerDay = 1151; // see https://beaconscan.com/stat/validatortotaldailyincome
-export const totalSupply = 117414000; // see https://etherscan.io/chart/ethersupplygrowth
+export const posIssuancePerDay = 1232; // see https://beaconscan.com/stat/validatortotaldailyincome
+export const totalSupply = 117664000; // see https://etherscan.io/chart/ethersupplygrowth
