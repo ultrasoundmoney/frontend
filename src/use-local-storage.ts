627cc8a7afe9e4040acd967186d31386aa40ecaf
Alexander Tesfamichael <alex.tesfamichael@gmail.com>
2021-11-17 16:34:04 +0000

    make use local storage safe

--- src/use-local-storage.ts
+++ src/use-local-storage.ts
@@ -6,9 +6,11 @@ export const useLocalStorage = <T>(key: string, initialValue: T) => {
   const [storedValue, setStoredValue] = useState<T>(() => {
     try {
       // Get from local storage by key
-      const item = window.localStorage.getItem(key);
-      // Parse stored json or if none return initialValue
-      return item ? JSON.parse(item) : initialValue;
+      if (typeof window !== "undefined") {
+        const item = window.localStorage.getItem(key);
+        // Parse stored json or if none return initialValue
+        return item ? JSON.parse(item) : initialValue;
+      }
     } catch (error) {
       // If error also return initialValue
       console.log(error);
@@ -25,7 +27,9 @@ export const useLocalStorage = <T>(key: string, initialValue: T) => {
       // Save state
       setStoredValue(valueToStore);
       // Save to local storage
-      window.localStorage.setItem(key, JSON.stringify(valueToStore));
+      if (typeof window !== "undefined") {
+        window.localStorage.setItem(key, JSON.stringify(valueToStore));
+      }
     } catch (error) {
       // A more advanced implementation would handle the error case
       console.log(error);
