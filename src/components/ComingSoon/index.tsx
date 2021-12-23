2d705bce2ec0a63f339bf75f777455c7c9947d3d
Alexander Tesfamichael <alex.tesfamichael@gmail.com>
2021-12-23 15:42:04 -0600

    add consistent styling for new section

--- src/components/ComingSoon/index.tsx
+++ src/components/ComingSoon/index.tsx
@@ -1,118 +1,122 @@
 import * as Sentry from "@sentry/react";
-import React, { FC, useContext } from "react";
-import { TranslationsContext } from "../../translations-context";
+import React, { FC } from "react";
 import FollowingYou from "../FollowingYou";
 import FaqBlock from "../Landing/faq";
 import SupplyView from "../SupplyView";
+import { SectionTitle } from "../Texts";
 import TwitterCommunity from "../TwitterCommunity";
 import Flippenings from "./Flippenings";
 import Scarcity from "./Scarcity";
 import TopBar from "./TopBar";
 import Widgets from "./Widgets";
 
-const ComingSoon: FC = () => {
-  const t = useContext(TranslationsContext);
+const SectionDivider: FC<{ title: string; subtitle: string }> = ({
+  title,
+  subtitle,
+}) => (
+  <>
+    <div className="h-32"></div>
+    <SectionTitle title={title} subtitle={subtitle} />
+    <div className="h-10 xl:h-16"></div>
+  </>
+);
 
-  return (
-    <div className="wrapper bg-blue-midnightexpress blurred-bg-image">
-      <div className="container m-auto">
-        <div className="px-4 md:px-16">
-          <TopBar />
+const ComingSoon: FC = () => (
+  <div className="wrapper bg-blue-midnightexpress blurred-bg-image">
+    <div className="container m-auto">
+      <div className="px-4 md:px-16">
+        <TopBar />
+      </div>
+      <div
+        className={`ultra-sound-text w-full pt-16 text-6xl md:text-7xl md:w-1/2 lg:w-5/6 lg:pt-16 m-auto mb-8`}
+      >
+        ultra sound awakening
+      </div>
+      <p className="font-inter text-blue-spindle text-xl md:text-2xl lg:text-3xl text-white text-center mb-16">
+        track ETH become ultra sound
+      </p>
+      <video
+        className="w-full md:w-3/6 lg:w-2/6 mx-auto -mt-32 -mb-4 pr-6 mix-blend-lighten"
+        playsInline
+        autoPlay
+        muted
+        loop
+        poster="/bat-no-wings.png"
+      >
+        <source src="/bat-no-wings.webm" type="video/webm; codecs='vp9'" />
+        <source src="/bat-no-wings.mp4" type="video/mp4" />
+      </video>
+      {/* <video */}
+      {/*   className="absolute left-0 -ml-24 top-8 md:top-128 lg:top-96 opacity-40 mix-blend-lighten" */}
+      {/*   playsInline */}
+      {/*   autoPlay */}
+      {/*   muted */}
+      {/*   loop */}
+      {/*   poster="/moving-orbs.jpg" */}
+      {/* > */}
+      {/*   <source src="/moving-orbs.mp4" type="video/mp4" /> */}
+      {/*   <source src="/moving-orbs.webm" type="video/webm; codecs='vp9'" /> */}
+      {/* </video> */}
+      <Sentry.ErrorBoundary
+        fallback={
+          <div className="font-roboto text-white text-xl text-center">
+            an unexpected exception occured, devs have been notified.
+          </div>
+        }
+      >
+        <Widgets />
+      </Sentry.ErrorBoundary>
+      <SectionDivider
+        title="monetary premium"
+        subtitle="the race to efficiently capture value"
+      />
+      <div className="flex items-start flex-col lg:flex-row lg:gap-0 gap-y-4 lg:gap-4 px-4 md:px-16">
+        <Scarcity />
+        <Flippenings />
+      </div>
+      <div className="flex flex-col px-4 md:px-16 mb-16">
+        <SectionDivider
+          title="project the supply"
+          subtitle="get ready for a peakening"
+        />
+        <div className="w-full md:m-auto relative bg-blue-tangaroa px-2 md:px-4 xl:px-12 py-4 md:py-8 xl:py-12 rounded-xl">
+          <SupplyView />
         </div>
         <div
-          className={`ultra-sound-text w-full pt-16 text-6xl md:text-7xl md:w-1/2 lg:w-5/6 lg:pt-16 m-auto mb-8`}
+          id="join-the-fam"
+          className="relative flex px-4 md:px-0 pt-8 pt-40 mb-16"
         >
-          ultra sound awakening
+          <div className="w-full relative flex flex-col items-center">
+            {/* <video */}
+            {/*   className="absolute w-1/2 right-0 -mt-16 opacity-40 mix-blend-lighten" */}
+            {/*   playsInline */}
+            {/*   autoPlay */}
+            {/*   muted */}
+            {/*   loop */}
+            {/*   poster="/bat-no-wings.png" */}
+            {/* > */}
+            {/*   <source */}
+            {/*     src="/moving-orbs.webm" */}
+            {/*     type="video/webm; codecs='vp9'" */}
+            {/*   /> */}
+            {/*   <source src="/moving-orbs.mp4" type="video/mp4" /> */}
+            {/* </video> */}
+            <TwitterCommunity />
+          </div>
         </div>
-        <p className="font-inter text-blue-spindle text-xl md:text-2xl lg:text-3xl text-white text-center mb-16">
-          track ETH become ultra sound
-        </p>
-        <video
-          className="w-full md:w-3/6 lg:w-2/6 mx-auto -mt-32 -mb-4 pr-6 mix-blend-lighten"
-          playsInline
-          autoPlay
-          muted
-          loop
-          poster="/bat-no-wings.png"
-        >
-          <source src="/bat-no-wings.webm" type="video/webm; codecs='vp9'" />
-          <source src="/bat-no-wings.mp4" type="video/mp4" />
-        </video>
-        {/* <video */}
-        {/*   className="absolute left-0 -ml-24 top-8 md:top-128 lg:top-96 opacity-40 mix-blend-lighten" */}
-        {/*   playsInline */}
-        {/*   autoPlay */}
-        {/*   muted */}
-        {/*   loop */}
-        {/*   poster="/moving-orbs.jpg" */}
-        {/* > */}
-        {/*   <source src="/moving-orbs.mp4" type="video/mp4" /> */}
-        {/*   <source src="/moving-orbs.webm" type="video/webm; codecs='vp9'" /> */}
-        {/* </video> */}
-        <Sentry.ErrorBoundary
-          fallback={
-            <div className="font-roboto text-white text-xl text-center">
-              an unexpected exception occured, devs have been notified.
-            </div>
-          }
-        >
-          <Widgets />
-        </Sentry.ErrorBoundary>
-        <div className="h-32"></div>
-        <h1 className="text-white font-light text-center text-2xl md:text-3xl xl:text-41xl mb-8">
-          monetary premium
-        </h1>
-        <div className="h-4"></div>
-        <div className="flex items-start flex-col lg:flex-row lg:gap-0 gap-y-4 lg:gap-4 px-4 md:px-16">
-          <Scarcity />
-          <Flippenings />
+        <div className="flex px-4 md:px-0 pt-20 pb-20">
+          <div className="w-full lg:w-2/3 md:m-auto relative">
+            <FollowingYou />
+          </div>
         </div>
-        <div className="flex flex-col px-4 md:px-16 pt-40 mb-16">
-          <h1 className="text-white font-light text-center text-2xl md:text-3xl xl:text-41xl mb-8">
-            {t.teaser_supply_title}
-          </h1>
-          <p className="text-blue-shipcove text-center font-light text-base lg:text-lg mb-8">
-            {t.teaser_supply_subtitle}
-          </p>
-          <div className="w-full md:m-auto relative bg-blue-tangaroa px-2 md:px-4 xl:px-12 py-4 md:py-8 xl:py-12 rounded-xl">
-            <SupplyView />
-          </div>
-          <div
-            id="join-the-fam"
-            className="relative flex px-4 md:px-0 pt-8 pt-40 mb-16"
-          >
-            <div className="w-full relative flex flex-col items-center">
-              {/* <video */}
-              {/*   className="absolute w-1/2 right-0 -mt-16 opacity-40 mix-blend-lighten" */}
-              {/*   playsInline */}
-              {/*   autoPlay */}
-              {/*   muted */}
-              {/*   loop */}
-              {/*   poster="/bat-no-wings.png" */}
-              {/* > */}
-              {/*   <source */}
-              {/*     src="/moving-orbs.webm" */}
-              {/*     type="video/webm; codecs='vp9'" */}
-              {/*   /> */}
-              {/*   <source src="/moving-orbs.mp4" type="video/mp4" /> */}
-              {/* </video> */}
-              <TwitterCommunity />
-            </div>
-          </div>
-          <div className="flex px-4 md:px-0 pt-20 pb-20">
-            <div className="w-full lg:w-2/3 md:m-auto relative">
-              <FollowingYou />
-            </div>
-          </div>
-          <div className="flex px-4 md:px-0 pt-8 pb-60">
-            <div className="w-full lg:w-2/3 md:m-auto relative">
-              <FaqBlock />
-            </div>
+        <div className="flex px-4 md:px-0 pt-8 pb-60">
+          <div className="w-full lg:w-2/3 md:m-auto relative">
+            <FaqBlock />
           </div>
         </div>
       </div>
     </div>
-  );
-};
+  </div>
+);
 
 export default ComingSoon;
--- src/components/Texts.tsx
+++ src/components/Texts.tsx
@@ -18,3 +18,19 @@ export const UnitText: FC = ({ children }) => (
     {children}
   </span>
 );
+
+export const SectionTitle: FC<{ title: string; subtitle: string }> = ({
+  title,
+  subtitle,
+}) => {
+  return (
+    <>
+      <h2 className="text-white font-light text-center text-2xl md:text-3xl xl:text-41xl mb-6">
+        {title}
+      </h2>
+      <p className="text-blue-shipcove text-center font-light text-base lg:text-lg">
+        {subtitle}
+      </p>
+    </>
+  );
+};
