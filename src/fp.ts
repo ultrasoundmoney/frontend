9da2144c67eda1a8f8c55b787436d9cf66b95f03
Alexander Tesfamichael <alex.tesfamichael@gmail.com>
2022-01-09 15:13:04 -0600

    add option string from unknown

--- src/fp.ts
+++ src/fp.ts
@@ -1,5 +1,6 @@
 import * as Num from "fp-ts/number";
 import * as O from "fp-ts/Option";
+import * as Str from "fp-ts/string";
 
 export { flow, pipe } from "fp-ts/function";
 export * as O from "fp-ts/Option";
@@ -7,4 +8,5 @@ export * as A from "fp-ts/Array";
 
 export const OAlt = {
   numberFromUnknown: O.fromPredicate(Num.isNumber),
+  stringFromUnknown: O.fromPredicate(Str.isString),
 };
