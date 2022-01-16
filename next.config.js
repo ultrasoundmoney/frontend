1fe707478c2e91c3707a15fcd6ebbc17e81c6253
Alexander Tesfamichael <alex.tesfamichael@gmail.com>
2022-01-16 21:30:52 +0100

    refactor: use pipe over flow
    Pipe is more straightforward. No compose before calling with the result.

--- next.config.js
+++ next.config.js
@@ -1,19 +1,23 @@
-const { flow } = require("lodash");
+const { pipe } = require("fp-ts/function");
 const withBundleAnalyzer = require("@next/bundle-analyzer")({
   enabled: process.env.ANALYZE === "true",
 });
 const withImages = require("next-images");
-module.exports = flow([withBundleAnalyzer, withImages])({
-  // Fleek recommends this.
-  // See: https://blog.fleek.co/posts/fleek-nextJS
-  trailingSlash: true,
-  // Without the exportPathMap fleek deployments for /page break.
-  exportPathMap: () => ({
-    "/": { page: "/" },
-    "/landing": { page: "/landing" },
-    "/dashboard": { page: "/dashboard" },
-  }),
-  images: {
-    disableStaticImages: true,
+module.exports = pipe(
+  {
+    // Fleek recommends this.
+    // See: https://blog.fleek.co/posts/fleek-nextJS
+    trailingSlash: true,
+    // Without the exportPathMap fleek deployments for /page break.
+    exportPathMap: () => ({
+      "/": { page: "/" },
+      "/landing": { page: "/landing" },
+      "/dashboard": { page: "/dashboard" },
+    }),
+    images: {
+      disableStaticImages: true,
+    },
   },
-});
+  withBundleAnalyzer,
+  withImages
+);
