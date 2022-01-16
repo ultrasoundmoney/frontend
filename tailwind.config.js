6b9ce66639db1a957941baa59befec4250ac94d9
Alexander Tesfamichael <alex.tesfamichael@gmail.com>
2022-01-16 23:47:28 +0100

    fix: opacity purgecss exception

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,18 @@ module.exports = {
       "./src/pages/**/*.{js,ts,jsx,tsx}",
       "./src/components/**/*.{js,ts,jsx,tsx}",
     ],
-    safeList: [/opacity-\d+/],
+    safeList: [
+      "opacity-10",
+      "opacity-20",
+      "opacity-30",
+      "opacity-40",
+      "opacity-50",
+      "opacity-60",
+      "opacity-70",
+      "opacity-80",
+      "opacity-90",
+      "opacity-100",
+    ],
   },
   theme: {
     extend: {
