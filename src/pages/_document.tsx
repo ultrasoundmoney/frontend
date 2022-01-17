9df5c89269a1c9311ebd0589d829c7422b9fe3fe
Alexander Tesfamichael <alex.tesfamichael@gmail.com>
2022-01-17 15:38:32 +0100

    refactor: move font imports into _document

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,7 +5,20 @@ class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
-        <Head />
+        <Head>
+          <link
+            href="https://fonts.googleapis.com/css2?family=Roboto+Mono:wght@100;200;300;400&display=swap"
+            rel="stylesheet"
+          />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400&display=swap"
+            rel="stylesheet"
+          />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
         <body>
           <Main />
           <NextScript />
