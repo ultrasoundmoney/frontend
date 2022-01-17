ccf34fa4d85713cfdf3015f9baf894904abd8755
Alexander Tesfamichael <alex.tesfamichael@gmail.com>
2022-01-17 18:09:44 +0100

    refactor: merge global styles
    We had over a dozen _global_ style modules. Hard to keep track of their
    effects. Hard to modify. Hard to remove bits from. They entangled
    components that shouldn't be. They only grow. This commits merges
    those modules. There is a lot of duplication. Many bits should be
    extracted to component local alternatives, many bits are unused and can
    be removed, some bits should be abstract components.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,8 +2,8 @@ import { AppProps } from "next/app";
 import * as Sentry from "@sentry/react";
 import { Integrations } from "@sentry/tracing";
 import "tailwindcss/tailwind.css";
-import "../../styles/index.css";
-import "../../styles/base.scss";
+import "../styles/index.css";
+import "../styles/global.scss";
 
 Sentry.init({
   dsn: "https://3ef07f75a826463a94510b8d676fd7e3@o920717.ingest.sentry.io/5866579",
