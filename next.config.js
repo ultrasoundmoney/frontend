c61687111f1b9565c1b93e035694b78b84c991cc
Alexander Tesfamichael <alex.tesfamichael@gmail.com>
2022-01-17 02:54:47 +0100

    chore: remove bundle-analyzer and with-images
    We haven't analyzed the bundle in a while. with-images no longer seems
    necessary after upgrading next.

--- next.config.js
+++ next.config.js
@@ -1,23 +1,12 @@
-const { pipe } = require("fp-ts/function");
-const withBundleAnalyzer = require("@next/bundle-analyzer")({
-  enabled: process.env.ANALYZE === "true",
-});
-const withImages = require("next-images");
-module.exports = pipe(
-  {
-    // Fleek recommends this.
-    // See: https://blog.fleek.co/posts/fleek-nextJS
-    trailingSlash: true,
-    // Without the exportPathMap fleek deployments for /page break.
-    exportPathMap: () => ({
-      "/": { page: "/" },
-      "/landing": { page: "/landing" },
-      "/dashboard": { page: "/dashboard" },
-    }),
-    images: {
-      disableStaticImages: true,
-    },
-  },
-  withBundleAnalyzer,
-  withImages
-);
+/** @type {import('next').NextConfig} */
+module.exports = {
+  // Fleek recommends this.
+  // See: https://blog.fleek.co/posts/fleek-nextJS
+  trailingSlash: true,
+  // Without the exportPathMap fleek deployments for /page break.
+  exportPathMap: () => ({
+    "/": { page: "/" },
+    "/landing": { page: "/landing" },
+    "/dashboard": { page: "/dashboard" },
+  }),
+};
