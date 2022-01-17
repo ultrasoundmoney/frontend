bf939af814df60b910dcc62a765645ff26396361
Alexander Tesfamichael <alex.tesfamichael@gmail.com>
2022-01-17 19:53:17 +0100

    refactor: remove duplicate tailwindcss import

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,6 @@ import { AppProps } from "next/app";
 import * as Sentry from "@sentry/react";
 import { Integrations } from "@sentry/tracing";
 import "tailwindcss/tailwind.css";
-import "../styles/index.css";
 import "../styles/global.scss";
 
 Sentry.init({
