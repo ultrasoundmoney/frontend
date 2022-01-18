0adaff224da15786c5d450c5562d27d7a3eaa752
Alexander Tesfamichael <alex.tesfamichael@gmail.com>
2022-01-18 02:24:05 +0100

    refactor: remove unused format fn

--- src/format.ts
+++ src/format.ts
@@ -1,26 +1,6 @@
 import { pipe } from "fp-ts/lib/function";
 import JSBI from "jsbi";
 
-export function formatLargeNumber(
-  n: number,
-  precision: number,
-  i18n: Data
-): string {
-  let abbrev;
-  if (n >= 1e6) {
-    n = n / 1e6;
-    abbrev = i18n.numeric_billion_abbrev;
-  } else if (n >= 1e3) {
-    n = n / 1e3;
-    abbrev = i18n.numeric_thousand_abbrev;
-  }
-  if (precision !== undefined) {
-    // Need to parseFloat again to avoid scientific notation
-    n = parseFloat(n.toPrecision(precision));
-  }
-  return abbrev ? `${n}${abbrev}` : String(n);
-}
-
 const intlFormatter = new Intl.NumberFormat();
 export function intlFormat(num: number): string {
   return intlFormatter.format(num);
