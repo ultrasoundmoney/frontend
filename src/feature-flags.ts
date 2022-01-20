e451c7099c54b578f05d57dca4d7b7157f3d321b
Alexander Tesfamichael <alex.tesfamichael@gmail.com>
2022-01-20 19:44:28 +0100

    refactor: remove unused feature flag

--- src/feature-flags.ts
+++ src/feature-flags.ts
@@ -2,5 +2,4 @@ import * as Config from "./config";
 
 export const featureFlags = {
   enableCategories: Config.env === "dev" || Config.env === "staging",
-  flippenings: Config.env === "dev" || Config.env === "staging",
 };
