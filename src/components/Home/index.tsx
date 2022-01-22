8a7b8007e361e1e6020ec7068ac217a380dd5af6
Alexander Tesfamichael <alex.tesfamichael@gmail.com>
2022-01-22 13:23:27 +0100

    fix: remove break from main title on mobile

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -32,7 +32,7 @@ const Title: FC = ({ children }) => (
       font-extralight
       text-white text-center
       mt-16 mb-8 mx-auto px-4 md:px-16
-      text-[4.8rem]
+      text-[4.6rem]
       md:text-[4.0rem]
       lg:text-[4.8rem]
       leading-[5.4rem]
