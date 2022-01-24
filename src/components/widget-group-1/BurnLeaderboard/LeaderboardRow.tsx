c01f326a99956f555de4db6ae23070e829710216
Alexander Tesfamichael <alex.tesfamichael@gmail.com>
2022-01-24 03:03:40 +0100

    fix: use of admin token

--- src/components/widget-group-1/BurnLeaderboard/LeaderboardRow.tsx
+++ src/components/widget-group-1/BurnLeaderboard/LeaderboardRow.tsx
@@ -2,6 +2,7 @@ import * as DateFns from "date-fns";
 import { FC, ReactEventHandler, useCallback } from "react";
 import CountUp from "react-countup";
 import Skeleton from "react-loading-skeleton";
+import { useAdminToken } from "../../../admin";
 import * as Contracts from "../../../api/contracts";
 import { LeaderboardEntry } from "../../../api/leaderboards";
 import { Unit } from "../../../denomination";
@@ -9,28 +10,31 @@ import { useFeatureFlags } from "../../../feature-flags";
 import * as Format from "../../../format";
 import { AmountUnitSpace } from "../../Spacing";
 
-const onSetTwitterHandle = async (address: string) => {
+const onSetTwitterHandle = async (
+  address: string,
+  token: string | undefined,
+) => {
   const handle = window.prompt(`input twitter handle`);
   if (handle === null) {
     return;
   }
-  await Contracts.setContractTwitterHandle(address, handle);
+  await Contracts.setContractTwitterHandle(address, handle, token);
 };
 
-const onSetName = async (address: string) => {
+const onSetName = async (address: string, token: string | undefined) => {
   const nameInput = window.prompt(`input name`);
   if (nameInput === null) {
     return;
   }
-  await Contracts.setContractName(address, nameInput);
+  await Contracts.setContractName(address, nameInput, token);
 };
 
-const onSetCategory = async (address: string) => {
+const onSetCategory = async (address: string, token: string | undefined) => {
   const category = window.prompt(`input category`);
   if (category === null) {
     return;
   }
-  await Contracts.setContractCategory(address, category);
+  await Contracts.setContractCategory(address, category, token);
 };
 
 const getOpacityFromAge = (dt: Date | undefined) =>
@@ -44,70 +48,76 @@ const getOpacityFromAge = (dt: Date | undefined) =>
 const AdminControls: FC<{
   address: string;
   freshness: Contracts.MetadataFreshness | undefined;
-}> = ({ address, freshness }) => (
-  <>
-    <div className="flex flex-row gap-4">
-      <a
-        className="text-pink-300 hover:opacity-60 hover:text-pink-300 cursor-pointer"
-        onClick={() => onSetTwitterHandle(address)}
-        target="_blank"
-        rel="noreferrer"
-      >
-        set handle
-      </a>
-      <a
-        className="text-pink-300 hover:opacity-60 hover:text-pink-300 cursor-pointer"
-        onClick={() => onSetName(address)}
-        target="_blank"
-        rel="noreferrer"
-      >
-        set name
-      </a>
-      <a
-        className="text-pink-300 hover:opacity-60 hover:text-pink-300 cursor-pointer"
-        onClick={() => onSetCategory(address)}
-        target="_blank"
-        rel="noreferrer"
-      >
-        set category
-      </a>
-      <a
-        className="text-pink-300 hover:opacity-60 hover:text-pink-300 cursor-pointer"
-        onClick={() => Contracts.setContractLastManuallyVerified(address)}
-        target="_blank"
-        rel="noreferrer"
-      >
-        set verified
-      </a>
-    </div>
-    <div className="flex text-sm text-white gap-x-4 mt-2">
-      <span
-        className="bg-gray-700 rounded-lg py-1 px-2"
-        style={{
-          opacity: getOpacityFromAge(freshness?.openseaContractLastFetch),
-        }}
-      >
-        {freshness?.openseaContractLastFetch === undefined
-          ? "never fetched"
-          : `opensea fetch ${DateFns.formatDistanceToNowStrict(
-              freshness.openseaContractLastFetch,
-            )} ago`}
-      </span>
-      <span
-        className="bg-gray-700 rounded-lg py-1 px-2"
-        style={{
-          opacity: getOpacityFromAge(freshness?.lastManuallyVerified),
-        }}
-      >
-        {freshness?.lastManuallyVerified === undefined
-          ? "never verified"
-          : `last verified ${DateFns.formatDistanceToNowStrict(
-              freshness.lastManuallyVerified,
-            )} ago`}
-      </span>
-    </div>
-  </>
-);
+}> = ({ address, freshness }) => {
+  const adminToken = useAdminToken();
+
+  return (
+    <>
+      <div className="flex flex-row gap-4">
+        <a
+          className="text-pink-300 hover:opacity-60 hover:text-pink-300 cursor-pointer"
+          onClick={() => onSetTwitterHandle(address, adminToken)}
+          target="_blank"
+          rel="noreferrer"
+        >
+          set handle
+        </a>
+        <a
+          className="text-pink-300 hover:opacity-60 hover:text-pink-300 cursor-pointer"
+          onClick={() => onSetName(address, adminToken)}
+          target="_blank"
+          rel="noreferrer"
+        >
+          set name
+        </a>
+        <a
+          className="text-pink-300 hover:opacity-60 hover:text-pink-300 cursor-pointer"
+          onClick={() => onSetCategory(address, adminToken)}
+          target="_blank"
+          rel="noreferrer"
+        >
+          set category
+        </a>
+        <a
+          className="text-pink-300 hover:opacity-60 hover:text-pink-300 cursor-pointer"
+          onClick={() =>
+            Contracts.setContractLastManuallyVerified(address, adminToken)
+          }
+          target="_blank"
+          rel="noreferrer"
+        >
+          set verified
+        </a>
+      </div>
+      <div className="flex text-sm text-white gap-x-4 mt-2">
+        <span
+          className="bg-gray-700 rounded-lg py-1 px-2"
+          style={{
+            opacity: getOpacityFromAge(freshness?.openseaContractLastFetch),
+          }}
+        >
+          {freshness?.openseaContractLastFetch === undefined
+            ? "never fetched"
+            : `opensea fetch ${DateFns.formatDistanceToNowStrict(
+                freshness.openseaContractLastFetch,
+              )} ago`}
+        </span>
+        <span
+          className="bg-gray-700 rounded-lg py-1 px-2"
+          style={{
+            opacity: getOpacityFromAge(freshness?.lastManuallyVerified),
+          }}
+        >
+          {freshness?.lastManuallyVerified === undefined
+            ? "never verified"
+            : `last verified ${DateFns.formatDistanceToNowStrict(
+                freshness.lastManuallyVerified,
+              )} ago`}
+        </span>
+      </div>
+    </>
+  );
+};
 
 type Props = {
   address?: string;
