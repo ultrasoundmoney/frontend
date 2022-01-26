bbff0d30673aa59557120fb01eea8a96d62c82e2
Alexander Tesfamichael <alex.tesfamichael@gmail.com>
2022-01-26 18:31:23 +0100

    refactor: select time frame outside hook

--- src/api/eth_price.ts
+++ src/api/eth_price.ts
@@ -14,9 +14,7 @@ export const useEthPrice = (): EthPrice | undefined => {
 
 export type AverageEthPrice = Record<TimeFrameNext, number>;
 
-export const useAverageEthPrice = (
-  timeFrame: TimeFrameNext,
-): number | undefined => {
+export const useAverageEthPrice = (): AverageEthPrice | undefined => {
   const { data } = useSWR<AverageEthPrice>(
     `${feesBasePath}/average-eth-price`,
     {
@@ -24,5 +22,5 @@ export const useAverageEthPrice = (
     },
   );
 
-  return data === undefined ? undefined : data[timeFrame];
+  return data === undefined ? undefined : data;
 };
--- src/components/Gauges/IssuanceGauge.tsx
+++ src/components/Gauges/IssuanceGauge.tsx
@@ -13,18 +13,21 @@ type Props = {
 };
 
 const IssuanceGauge: FC<Props> = ({ simulateMerge, timeFrame, unit }) => {
-  const averageEthPrice = useAverageEthPrice(timeFrame);
+  const averageEthPrice = useAverageEthPrice();
+
+  const selectedAverageEthPrice =
+    averageEthPrice === undefined ? undefined : averageEthPrice[timeFrame];
 
   const issuancePerDay = simulateMerge
     ? StaticEtherData.posIssuancePerDay
     : StaticEtherData.powIssuancePerDay + StaticEtherData.posIssuancePerDay;
 
   const issuance =
-    averageEthPrice === undefined
+    selectedAverageEthPrice === undefined
       ? 0
       : unit === "eth"
       ? (issuancePerDay * 365.25) / 1_000_000
-      : (issuancePerDay * 365.25 * averageEthPrice) / 1_000_000_000;
+      : (issuancePerDay * 365.25 * selectedAverageEthPrice) / 1_000_000_000;
 
   return (
     <div className="flex flex-col justify-start items-center bg-blue-tangaroa px-4 md:px-0 py-8 pt-7 rounded-lg md:rounded-l-none lg:rounded-l-lg">
