fc375d96f0ecbcbece46a10956bf4ea22c049650
OlegLomaka <lomacka.oleh@gmail.com>
2022-01-31 15:41:07 +0200

    git commit -m "#24 implement animated path with framer-motion"

--- src/components/Landing/AnimatedPath.tsx
+++ src/components/Landing/AnimatedPath.tsx
@@ -1,143 +1,227 @@
-import React, { useCallback, useEffect, useState } from "react";
-import staticPath from "../../assets/lines-bg.svg";
-import blurredBg from "../../assets/blurred-bg1.svg";
-import { throttle } from "lodash";
+import React, { useEffect, useRef, useState } from "react";
+import { motion, useSpring, useMotionValue } from "framer-motion";
+// import glowBg from "../../assets/blurred-bg1.svg";
+import glowBg from "../../assets/blurred-bg1.png";
 
-function getPrecent(ref: HTMLInputElement, topOffset = 400) {
-  const rect = ref.getBoundingClientRect();
-  let percent = 0;
-  percent = 100 - ((rect.bottom - topOffset) / rect.height) * 100;
-  return percent > 100 ? 100 : percent;
-}
+const isElementInViewport = (el: SVGSVGElement) => {
+  const rect = el.getBoundingClientRect();
+  const offset = window.innerHeight / 2;
+  const start = rect.top - offset;
+  const end = rect.bottom;
 
-// function isElementInViewport(ref: HTMLInputElement, topOffset = 400) {
-//   const rect = ref.getBoundingClientRect();
-//   return rect.bottom - rect.height - topOffset < 0 && rect.bottom > 0;
-// }
+  return start < 0 && end > 0;
+};
+
+const points = {
+  left: "20 20 20 800",
+  right: "120 300 120 800 20 840 20 920",
+};
+const dots = [
+  {
+    x: 20,
+    y: 20,
+    animatedOn: 0,
+    isAnimated: false,
+  },
+  {
+    x: 20,
+    y: 215,
+    animatedOn: 0.27,
+    isAnimated: false,
+  },
+  {
+    x: 20,
+    y: 410,
+    animatedOn: 0.51,
+    isAnimated: false,
+  },
+  {
+    x: 20,
+    y: 605,
+    animatedOn: 0.78,
+    isAnimated: false,
+  },
+  {
+    x: 20,
+    y: 800,
+    animatedOn: 0.98,
+    isAnimated: false,
+  },
+
+  {
+    x: 120,
+    y: 300,
+    animatedOn: 0,
+    isAnimated: false,
+  },
+  {
+    x: 120,
+    y: 600,
+    animatedOn: 0.45,
+    isAnimated: false,
+  },
+  {
+    x: 20,
+    y: 840,
+    animatedOn: 0.92,
+    isAnimated: false,
+  },
+];
+const dotVariants = {
+  visible: { opacity: 1, scale: 1, duration: ".2s" },
+  hidden: { opacity: 0, scale: 0.1, duration: ".2s" },
+};
+const glowVariants = {
+  visible: { opacity: 1, duration: ".2s", transition: { delay: 0.5 } },
+  hidden: { opacity: 0, duration: ".2s" },
+};
 
 const AnimatedPath: React.FC<{}> = () => {
-  const [scrollPosition, setScrollPosition] = useState(0);
-  const [scrollProgress, setScrollProgress] = useState(0);
-  const [pathState, setPathState] = useState({
-    left: "20 20 20 800",
-    right: "200 300 200 800 20 836 20 920",
+  const pathRef = useRef<null | SVGSVGElement>(null);
+  const [dotsState, setDotsState] = useState(dots);
+  const [glowIsShow, setGlowIsShow] = useState(false);
+  const [scrollYProgress, setScrollYProgress] = useState(0);
+  const animationYProgress = useMotionValue(0);
+  const pathLength = useSpring(animationYProgress, {
+    stiffness: 400,
+    damping: 90,
   });
-  const [ref, setRef] = useState<HTMLInputElement | null>(null);
-  const pointsArr = [
-    [20, 20],
-    // [20, 260],
-    // [20, 520],
-    // [20, 800],
-    [200, 300],
-    // [200, 568],
-    // [20, 836],
-  ];
 
-  const handlePath = () => {
-    const offset = 20;
-    const lh = 800;
-    const calcLh = (scrollProgress * lh) / 100 - offset;
-    setPathState((prevState) => ({
-      ...prevState,
-      left: `20 20 20 ${calcLh > offset ? calcLh : 21}`,
-    }));
-    console.log(scrollProgress);
-    console.log(calcLh);
-    console.log(pathState);
+  const getScrollProgress = (el: SVGSVGElement) => {
+    const rect = el.getBoundingClientRect();
+    const offset = window.innerHeight / 2;
+    if (rect.y - offset < 0) {
+      let progress = ((rect.y - offset) * -1) / rect.height;
+      const lastProgress = animationYProgress.get();
+      if (progress < lastProgress) {
+        progress = lastProgress;
+      }
+      return progress > 1 ? 1 : progress;
+    }
+    return 0;
   };
-
-  const onRefSet = useCallback((ref) => {
-    setRef(ref);
-  }, []);
 
   const handleScroll = () => {
-    setScrollPosition(window.scrollY);
+    setScrollYProgress(window.scrollY);
   };
 
-  const throttledHandleScroll = useCallback(
-    throttle(() => handleScroll(), 10),
-    []
-  );
-
   useEffect(() => {
-    window.addEventListener("scroll", throttledHandleScroll);
+    window.addEventListener("scroll", handleScroll);
 
     return () => {
-      window.removeEventListener("scroll", throttledHandleScroll);
+      window.removeEventListener("scroll", handleScroll);
     };
   }, []);
 
   useEffect(() => {
-    if (ref) {
-      const topOffset = window.innerHeight / 2;
-      // if (isElementInViewport(ref, topOffset)) {
-      const precent = getPrecent(ref, topOffset);
-      // precent > scrollProgress &&
-      setScrollProgress(precent);
-      handlePath();
-      // }
-    }
-  }, [scrollPosition, ref]);
+    if (pathRef.current)
+      if (isElementInViewport(pathRef.current)) {
+        const pathLengthProgress = pathLength.get();
+        // if animated done
+        if (pathLengthProgress === 1) return;
+
+        // get event scroll progress
+        const progress = getScrollProgress(pathRef.current);
+
+        // set progress value
+        animationYProgress.set(progress);
+
+        // show green points
+        setDotsState((prevState) => {
+          return prevState.map((dot) =>
+            dot.animatedOn < pathLengthProgress
+              ? { ...dot, isAnimated: true }
+              : dot
+          );
+        });
+
+        // forced animated
+        if (pathLengthProgress > 0.7) {
+          animationYProgress.set(1);
+          setDotsState((prevState) =>
+            prevState.map((dot) => ({ ...dot, isAnimated: true }))
+          );
+          // show glow
+          setGlowIsShow(true);
+        }
+      }
+  }, [scrollYProgress, pathRef]);
 
   return (
     <div className="merge-path">
-      <div className="merge-path_static">
-        <img className="bg" src={blurredBg} alt="img" />
-        <img className="path" src={staticPath} alt="img" />
-      </div>
-      <svg
-        ref={onRefSet}
-        viewBox="0 0 210 938"
-        width="220"
-        height="938"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <polyline
-          points={pathState.left}
-          stroke="#191F32"
-          fill="none"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          strokeWidth={24}
-          style={{ transition: "1s" }}
+      <div className="merge-path_animated">
+        <motion.img
+          className="bg"
+          src={glowBg}
+          alt="glow"
+          variants={glowVariants}
+          animate={glowIsShow ? "visible" : "hidden"}
         />
-        <polyline
-          points={pathState.left}
-          stroke="#ffffff"
-          fill="none"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          strokeDasharray="4 8"
-          strokeWidth=".4"
-          style={{ transition: "1s" }}
-        />
-        <polyline
-          points={pathState.right}
-          stroke="#191F32"
-          fill="none"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          strokeWidth={24}
-        />
-        <polyline
-          points={pathState.right}
-          stroke="#ffffff"
-          fill="none"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          strokeDasharray="4 8"
-          strokeWidth=".4"
-        />
-        {pointsArr.map((point, index) => (
-          <circle
-            key={index}
-            fill="#00FFA3"
-            cx={point[0]}
-            cy={point[1]}
-            r="4"
+        <motion.svg
+          className="path"
+          ref={pathRef}
+          xmlns="http://www.w3.org/2000/svg"
+          width="210"
+          height="938"
+          style={{ margin: "auto" }}
+        >
+          <motion.polyline
+            points={points.left}
+            fill="transparent"
+            strokeWidth="20"
+            stroke="#191F32"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            style={{
+              pathLength: pathLength,
+            }}
           />
-        ))}
-      </svg>
+          <motion.polyline
+            points={points.right}
+            fill="transparent"
+            strokeWidth="20"
+            stroke="#191F32"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            style={{
+              pathLength: pathLength,
+            }}
+          />
+          <motion.polyline
+            points={points.left}
+            fill="transparent"
+            strokeWidth=".5"
+            stroke="#8c8f98"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            style={{
+              pathLength: pathLength,
+            }}
+          />
+          <motion.polyline
+            points={points.right}
+            fill="transparent"
+            strokeWidth=".5"
+            stroke="#8c8f98"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            style={{
+              pathLength: pathLength,
+            }}
+          />
+          {dotsState.map((dot, index) => (
+            <motion.circle
+              key={index}
+              cx={dot.x}
+              cy={dot.y}
+              r="4"
+              fill="#00ffa3"
+              variants={dotVariants}
+              animate={dot.isAnimated ? "visible" : "hidden"}
+            />
+          ))}
+        </motion.svg>
+      </div>
     </div>
   );
 };
