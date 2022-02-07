cf874ad11d10fb8aa7eea9ba9e075064a0c470be
OlegLomaka <lomacka.oleh@gmail.com>
2022-02-07 16:53:05 +0200

    #33 EIP1559 video bg + fix drawing component

--- src/components/Landing/eip1559.tsx
+++ src/components/Landing/eip1559.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import AvatarImg from "../../assets/avatar.webp";
 import EthBurn from "../../assets/eth-burn.svg";
+import posterBg from "../../assets/bat-bg/Blurredbg.png";
 import { StepperContext } from "../../context/StepperContext";
 import { TranslationsContext } from "../../translations-context";
 import DrawingLine from "./DrawingLine";
@@ -26,15 +27,24 @@ const EIP1559: React.FC<{}> = () => {
       <DrawingLine pointRef={EIPRef} />
       <section
         data-aos="fade-up"
-        data-aos-anchor-placement="top-bottom"
+        data-aos-anchor-placement="top-center"
         data-aos-offset="100"
         data-aos-delay="50"
         data-aos-duration="1000"
         data-aos-easing="ease-in-out"
         id="eip-1559"
-        ref={EIPRef}
       >
         <div className="block-fee-burn flex flex-col justify-center items-center w-full md:w-6/12 md:mx-auto pt-20 px-4 md:px-0 ">
+          <video
+            className="eip_bg"
+            playsInline
+            autoPlay
+            muted
+            loop
+            poster={posterBg}
+          >
+            <source src="/eip_compressed.mp4" type="video/mp4" />
+          </video>
           <picture>
             <img
               className="relative text-center mx-auto mb-8"
