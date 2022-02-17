e651ce69a88211b2b28bc44969a071cd8568ee4a
Alexander Tesfamichael <alex.tesfamichael@gmail.com>
2022-02-17 15:04:27 +0100

    fix: emoji height in gauges

--- src/components/Gauges/IssuanceBurnBaseGauge.tsx
+++ src/components/Gauges/IssuanceBurnBaseGauge.tsx
@@ -50,7 +50,7 @@ const BaseGuage: FC<BaseGuageProps> = ({
 
   return (
     <>
-      <SpanMoji className="leading-10 text-4xl" emoji={emoji} />
+      <SpanMoji className="h-10" emoji={emoji} />
       <div className="mt-6 md:mt-2 lg:mt-8 transform scale-100 md:scale-75 lg:scale-100 xl:scale-110">
         <GaugeSvg
           progress={progress}
