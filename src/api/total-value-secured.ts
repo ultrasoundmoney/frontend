064a57ddb60f81679973d0941b3d152559807ba5
Alexander Tesfamichael <alex.tesfamichael@gmail.com>
2022-03-28 21:17:54 +0100

    fix: missing type

--- src/api/total-value-secured.ts
+++ src/api/total-value-secured.ts
@@ -1,6 +1,7 @@
 import useSWR from "swr";
 import * as Duration from "../duration";
 import fetcher from "./default-fetcher";
+import { Linkables } from "./fam";
 import { feesBasePath } from "./fees";
 
 export type TvsRanking = {
@@ -9,6 +10,7 @@ export type TvsRanking = {
   famFollowerCount: number | undefined;
   followerCount: number | undefined;
   imageUrl: string | undefined;
+  links: Linkables | undefined;
   marketCap: number;
   name: string;
   nftGoUrl: string | undefined;
