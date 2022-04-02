03f9909057d9fae27cdff211b35c125d7b831fbd
Alexander Tesfamichael <alex.tesfamichael@gmail.com>
2022-04-02 21:15:39 +0100

    fix: modal appearing below some elements

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -8,7 +8,7 @@ const Modal: FC<{
     className={`
       fixed top-0 left-0 bottom-0 right-0
       flex justify-center items-center
-      z-10
+      z-20
       bg-blue-highlightbg/60
       ${show ? "" : "hidden"}`}
     onClick={onClickBackground}
