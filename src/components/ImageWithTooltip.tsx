dde9cf02136addab4730aee2ece3409b783d40e5
Alexander Tesfamichael <alex.tesfamichael@gmail.com>
2022-05-12 12:43:30 +0200

    feat: add tooltip to burn leaderboard

--- src/components/ImageWithTooltip.tsx
+++ src/components/ImageWithTooltip.tsx
@@ -7,28 +7,16 @@ import {
   useRef,
 } from "react";
 import Skeleton from "react-loading-skeleton";
-import { Linkables } from "../api/fam";
 import { FeatureFlagsContext } from "../feature-flags";
 
 type ImageWithTooltipProps = {
   className?: HTMLImageElement["className"];
-  coingeckoUrl?: string;
-  contractAddresses?: string[];
-  description: string | undefined;
-  famFollowerCount: number | undefined;
-  followerCount: number | undefined;
   imageUrl: string | undefined;
   isDoneLoading?: boolean;
-  links?: Linkables;
-  nftGoUrl?: string;
-  onClick: () => void;
-  onHover?: (hovering: boolean, ref: RefObject<HTMLImageElement>) => void;
+  onClick?: () => void;
   onMouseEnter?: (ref: RefObject<HTMLImageElement>) => void;
   onMouseLeave?: (ref: RefObject<HTMLImageElement>) => void;
   skeletonDiameter?: string;
-  title: string | undefined;
-  tooltipImageUrl: string | undefined;
-  twitterUrl?: string;
 };
 
 const ImageWithTooltip: FC<ImageWithTooltipProps> = ({
@@ -36,8 +24,8 @@ const ImageWithTooltip: FC<ImageWithTooltipProps> = ({
   imageUrl,
   isDoneLoading = true,
   onClick,
-  onMouseEnter = () => undefined,
-  onMouseLeave = () => undefined,
+  onMouseEnter,
+  onMouseLeave,
   skeletonDiameter = "32px",
 }) => {
   const imageRef = useRef<HTMLImageElement>(null);
@@ -65,16 +53,20 @@ const ImageWithTooltip: FC<ImageWithTooltipProps> = ({
             rounded-full
             active:brightness-125 md:active:brightness-100
             cursor-pointer md:cursor-auto
-            hover:opacity-60
+            ${onMouseEnter !== undefined ? "hover:opacity-60" : ""}
             ${className}
           `}
           src={imageUrl ?? "/leaderboard-images/question-mark-v2.svg"}
           alt="logo of an ERC20 token"
           onError={onImageError}
-          onClick={() => onClick()}
+          onClick={onClick}
           ref={imageRef}
-          onMouseEnter={() => onMouseEnter(imageRef)}
-          onMouseLeave={() => onMouseLeave(imageRef)}
+          onMouseEnter={() =>
+            onMouseEnter === undefined ? undefined : onMouseEnter(imageRef)
+          }
+          onMouseLeave={() =>
+            onMouseLeave === undefined ? undefined : onMouseLeave(imageRef)
+          }
         />
       )}
     </>
