4c6b4d43289a3cb132de63879f88aa62dcd50c9e
Alexander Tesfamichael <alex.tesfamichael@gmail.com>
2022-08-13 12:50:53 +0200

    refactor: prefer arrow shorthand

--- src/pages/storypreview.tsx
+++ src/pages/storypreview.tsx
@@ -2,14 +2,12 @@ import { NextPage } from "next";
 import Head from "next/head";
 import Story from "../components/Landing";
 
-const StoryPreview: NextPage = () => {
-  return (
-    <>
-      <Head>
-        <title>home - ultrasound.money</title>
-      </Head>
-      <Story />
-    </>
-  );
-};
+const StoryPreview: NextPage = () => (
+  <>
+    <Head>
+      <title>home - ultrasound.money</title>
+    </Head>
+    <Story />
+  </>
+);
 export default StoryPreview;
