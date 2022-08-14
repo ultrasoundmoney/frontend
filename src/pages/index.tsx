008003d04550528d3d57321fcefd25afc1d57e18
Alexander Tesfamichael <alex.tesfamichael@gmail.com>
2022-08-14 23:20:33 +0200

    refactor: simplify top-level pages

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,16 @@
-import { NextPage } from "next";
-import DashboardPage from "./dashboard";
+import type { NextPage } from "next";
+import Head from "next/head";
+import { feesBasePath } from "../api/fees";
+import type { GroupedAnalysis1F } from "../api/grouped-analysis-1";
+import { decodeGroupedAnalysis1 } from "../api/grouped-analysis-1";
+import Dashboard from "../components/Dashboard";
 
-const IndexPage: NextPage = DashboardPage;
-
+const IndexPage: NextPage<Props> = ({ groupedAnalysis1F }) => (
+  <>
+    <Head>
+      <title>dashboard | ultrasound.money</title>
+    </Head>
+    <Dashboard />
+  </>
+);
 export default IndexPage;
