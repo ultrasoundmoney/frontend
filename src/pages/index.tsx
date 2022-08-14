bd7b89abd0c0464d3991599c2efa8ae5985edd08
Alexander Tesfamichael <alex.tesfamichael@gmail.com>
2022-08-14 23:22:00 +0200

    fix: leftover static props bits

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,8 @@
 import type { NextPage } from "next";
 import Head from "next/head";
-import { feesBasePath } from "../api/fees";
-import type { GroupedAnalysis1F } from "../api/grouped-analysis-1";
-import { decodeGroupedAnalysis1 } from "../api/grouped-analysis-1";
 import Dashboard from "../components/Dashboard";
 
-const IndexPage: NextPage<Props> = ({ groupedAnalysis1F }) => (
+const IndexPage: NextPage = () => (
   <>
     <Head>
       <title>dashboard | ultrasound.money</title>
