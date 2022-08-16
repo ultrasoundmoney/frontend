5587ad7f1a2a0f3e6d54493bb93c652769695c21
Alexander Tesfamichael <alex.tesfamichael@gmail.com>
2022-08-16 19:01:49 +0200

    refactor: remove unused fns

--- src/format.ts
+++ src/format.ts
@@ -95,29 +95,6 @@ export const ethFromWeiBIUnsafe = (wei: JSBI): number =>
 
 export const ethFromGwei = (gwei: number): number => gwei / 1e9;
 
-export const gweiFromEth = (eth: number): number => eth * 1e9;
-
-export const followerCountConvert = (num: number) => {
-  if (num > 999 && num < 1000000) {
-    // convert to K for number from > 1000 < 1 million
-    return (num / 1000).toFixed(1) + "K";
-  } else if (num > 1000000) {
-    // convert to M for number from > 1 million
-    return (num / 1000000).toFixed(1) + "M";
-  } else if (num <= 999) {
-    // if value < 1000, nothing to do
-    return num;
-  }
-};
-
-export const convertDateStringReadable = (date: string | number) => {
-  if (date != undefined || date != null) {
-    const dateString = new Date(date).toDateString();
-    const dateStringToArr = dateString.split(" ");
-    return `${dateStringToArr[1]} ${dateStringToArr[2]},${dateStringToArr[3]}`;
-  }
-};
-
 export function convertToInternationalCurrencySystem(labelValue: number) {
   // Nine Zeroes for Billions
   return Math.abs(Number(labelValue)) >= 1.0e9
