8409f3a2514f5031eb1011660fc68d9d5f5c19bf
Alexander Tesfamichael <alex.tesfamichael@gmail.com>
2022-08-16 15:14:16 +0200

    Revert "feat: add hydration hack"
    This reverts commit 46f4ce1f184b5f91b3ca3f684c170dce6ef547e9.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,6 @@
 import type { NextPage } from "next";
-import dynamic from "next/dynamic";
 import Head from "next/head";
-// HACK: attempt to stop constant hydration errors from happening in prod whilst trying to uncover their source.
-const Dashboard = dynamic(() => import("../components/Dashboard"), {
-  ssr: false,
-});
+import Dashboard from "../components/Dashboard";
 
 const IndexPage: NextPage = () => (
   <>
