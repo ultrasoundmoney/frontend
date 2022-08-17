9ca78b4362623a9e8dc8bea571d82285597f1f56
Alexander Tesfamichael <alex.tesfamichael@gmail.com>
2022-08-17 20:48:41 +0200

    feat: keep SSR off

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,10 @@
 import type { NextPage } from "next";
+import dynamic from "next/dynamic";
 import Head from "next/head";
-import Dashboard from "../components/Dashboard";
+// HACK: attempt to stop constant hydration errors from happening in prod whilst trying to uncover their source.
+const Dashboard = dynamic(() => import("../components/Dashboard"), {
+  ssr: false,
+});
 
 const IndexPage: NextPage = () => (
   <>
