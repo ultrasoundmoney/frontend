1c996d565a1b36c17d709af1cb1ee2e9ef0400da
Alexander Tesfamichael <alex.tesfamichael@gmail.com>
2022-08-17 23:31:34 +0200

    feat: add emoji component

---
+++ src/components/Emoji/index.tsx
@@ -0,0 +1,57 @@
+import type { ImageProps, StaticImageData } from "next/image";
+import Image from "next/image";
+import type { FC } from "react";
+import linkSvg from "./svg-white/link.svg";
+import anchorSvg from "./svg-white/anchor.svg";
+import whiteCheckSvg from "./svg/white-check.svg";
+import seeNoEvilSvg from "./svg/see-no-evil.svg";
+import hearNoEvilSvg from "./svg/hear-no-evil.svg";
+import speakNoEvilSvg from "./svg/speak-no-evil.svg";
+
+type SupportedColoredEmoji =
+  | "hear-no-evil"
+  | "see-no-evil"
+  | "speak-no-evil"
+  | "white-check";
+
+type SupportedWhiteEmoji = "anchor" | "link";
+
+const coloredMap: Record<SupportedColoredEmoji, StaticImageData> = {
+  "hear-no-evil": hearNoEvilSvg as StaticImageData,
+  "see-no-evil": seeNoEvilSvg as StaticImageData,
+  "speak-no-evil": speakNoEvilSvg as StaticImageData,
+  "white-check": whiteCheckSvg as StaticImageData,
+} as const;
+
+const whiteMap: Record<SupportedWhiteEmoji, StaticImageData> = {
+  anchor: anchorSvg as StaticImageData,
+  link: linkSvg as StaticImageData,
+} as const;
+
+type WhiteEmojiProps = {
+  alt: ImageProps["alt"];
+  layout?: ImageProps["layout"];
+  name: SupportedWhiteEmoji;
+  width?: ImageProps["width"];
+};
+
+export const WhiteEmoji: FC<WhiteEmojiProps> = ({
+  alt,
+  layout,
+  name,
+  width,
+}) => <Image alt={alt} layout={layout} src={whiteMap[name]} width={width} />;
+
+type ColoredEmojiProps = {
+  alt: ImageProps["alt"];
+  layout?: ImageProps["layout"];
+  name: SupportedColoredEmoji;
+  width?: ImageProps["width"];
+};
+
+export const ColoredEmoji: FC<ColoredEmojiProps> = ({
+  alt,
+  layout,
+  name,
+  width,
+}) => <Image alt={alt} layout={layout} src={coloredMap[name]} width={width} />;
