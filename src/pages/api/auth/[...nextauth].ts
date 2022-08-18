73a812c99e612a8a83577457fe1b2821c2fc84df
Alexander Tesfamichael <alex.tesfamichael@gmail.com>
2022-08-18 23:05:33 +0200

    feat: use twitter oauth2

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -4,11 +4,11 @@ import TwitterProvider from "next-auth/providers/twitter";
 export default NextAuth({
   secret: process.env.SECRET,
   providers: [
-    // OAuth authentication providers
     TwitterProvider({
       // These are string | undefined during testing failing our CI build.
       clientId: process.env.TWITTER_CLIENT_ID as string,
       clientSecret: process.env.TWITTER_CLIENT_SECRET as string,
+      version: "2.0",
     }),
   ],
 });
