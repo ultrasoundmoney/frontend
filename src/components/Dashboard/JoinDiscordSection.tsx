f31157be92e2251d7bfaedb11c4a0952b8a31b5d
Alexander Tesfamichael <alex.tesfamichael@gmail.com>
2022-08-24 15:06:29 +0200

    feat: improved anchor spacing

--- src/components/Dashboard/JoinDiscordSection.tsx
+++ src/components/Dashboard/JoinDiscordSection.tsx
@@ -3,9 +3,9 @@ import JoinDiscordWidget from "../JoinDiscordWidget";
 import { SectionTitle } from "../TextsNext/SectionTitle";
 
 const JoinDiscordSection: FC = () => (
-  <div className="pt-32" id="join-discord">
+  <div className="mt-16" id="join-discord">
     <SectionTitle
-      className="mb-16"
+      className="py-16"
       link="join-discord"
       highlightGradient="bg-gradient-to-br from-indigo-400 to-indigo-700"
       highlightPhrase="Discord"
