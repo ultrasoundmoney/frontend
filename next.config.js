bf19b82a84dcfedf6890850d326e4293f98103bb
Alexander Tesfamichael <alex.tesfamichael@gmail.com>
2022-08-24 02:50:33 +0200

    feat: create default prod rewrite

--- next.config.js
+++ next.config.js
@@ -18,15 +18,27 @@ const nextConfig = withBundleAnalyzer({
     ],
   },
   rewrites: async () => [
+    // To test locally with the fam-analysis api running and a local tunnel to receive the oauth2 callback, use the below rewrite.
+    // {
+    //   source: "/api/:path*",
+    //   has: [
+    //     {
+    //       type: "host",
+    //       value: "oyfy.tunnelto.dev",
+    //     },
+    //   ],
+    //   destination: "http://127.0.0.1:8080/api/:path*",
+    // },
+    // To do frontend dev accepting that an oauth2 callback won't work, but without our auth routes blowing up, use the below rewrite.
     {
       source: "/api/:path*",
       has: [
         {
           type: "host",
-          value: "oyfy.tunnelto.dev",
+          value: "localhost",
         },
       ],
-      destination: "http://127.0.0.1:8080/api/:path*",
+      destination: "https://ultrasound.money/api/:path*",
     },
   ],
 });
