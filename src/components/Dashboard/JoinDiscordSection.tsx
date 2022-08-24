4e1484dc96f3a7f8fd008c326dd333109e404224
Alexander Tesfamichael <alex.tesfamichael@gmail.com>
2022-08-24 03:10:29 +0200

    feat: fancy Discord styling

--- src/components/Dashboard/JoinDiscordSection.tsx
+++ src/components/Dashboard/JoinDiscordSection.tsx
@@ -1,12 +1,38 @@
+import Link from "next/link";
 import type { FC } from "react";
+import { useActiveBreakpoint } from "../../utils/use-active-breakpoint";
+import { WhiteEmoji } from "../Emoji";
 import JoinDiscordWidget from "../JoinDiscordWidget";
 import { SectionTitle } from "../Texts";
 
-const JoinDiscordSection: FC = () => (
-  <div id="join-discord">
-    <SectionTitle link="join-discord" title="ultra sound discord" />
-    <JoinDiscordWidget />
-  </div>
-);
-
+const JoinDiscordSection: FC = () => {
+  const { md } = useActiveBreakpoint();
+  return (
+    <div id="join-discord">
+      <div className="flex justify-center items-center gap-x-4">
+        <Link href={`#join-discord`}>
+          <a className="flex items-center gap-x-4 text-white">
+            <h2
+              className={`
+              font-inter font-extralight
+              text-white text-center text-2xl md:text-3xl xl:text-4xl
+            `}
+            >
+              ultra sound{" "}
+              <span className="text-transparent bg-clip-text bg-gradient-to-br from-indigo-400 to-indigo-700">
+                Discord
+              </span>
+            </h2>
+            <WhiteEmoji
+              alt="emoji of a chain link symbolizing a section anchor for easy linking"
+              name="link"
+              width={md ? 24 : 16}
+            />
+          </a>
+        </Link>
+      </div>
+      <JoinDiscordWidget />
+    </div>
+  );
+};
 export default JoinDiscordSection;
