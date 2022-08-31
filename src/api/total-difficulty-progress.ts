f830144b4b68e8a08af8621fd72affd06c9e2bb7
Alexander Tesfamichael <alex.tesfamichael@gmail.com>
2022-08-31 19:13:49 +0200

    fix: refresh total difficulty

--- src/api/total-difficulty-progress.ts
+++ src/api/total-difficulty-progress.ts
@@ -34,7 +34,7 @@ export const useTotalDifficultyProgress = ():
   const { data } = useSWR<TotalDifficultyProgress>(
     "/api/v2/fees/total-difficulty-progress",
     fetchJson,
-    {},
+    { refreshInterval: 60 },
   );
 
   return data;
