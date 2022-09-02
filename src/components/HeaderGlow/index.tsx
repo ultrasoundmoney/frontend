96cdf4976417988293cc73affaa7998dc01c9546
Alexander Tesfamichael <alex.tesfamichael@gmail.com>
2022-09-03 01:04:06 +0200

    feat: extract and fine-tune header glow

--- src/components/HeaderGlow/index.tsx
+++ src/components/HeaderGlow/index.tsx
@@ -1,30 +1,46 @@
 import type { StaticImageData } from "next/image";
 import Image from "next/image";
 import type { FC } from "react";
-import headerGlowSvg from "../../assets/blurred-bg.svg";
+import headerGlowSvg from "./header-glow.svg";
 
 const HeaderGlow: FC = () => (
   <div
     className={`
-      absolute
-      w-full
-      h-[600px] -mt-[50px]
-      lg:h-[700px]
-      xl:h-[800px]
-      2xl:h-[1600px] 2xl:-mt-[310px]
+      absolute w-full top-0
+      h-[500px]
+      xs:h-[650px]
+      md:h-[700px]
+      lg:h-[800px]
+      xl:h-[900px]
+      2xl:h-[1200px]
+      overflow-hidden flex justify-center
       -z-10
-      select-none
     `}
   >
-    <Image
-      className=""
-      alt=""
-      src={headerGlowSvg as StaticImageData}
-      layout="fill"
-      priority
-      quality={100}
-      objectFit="cover"
-    />
+    <div
+      className={`
+        absolute
+        w-full
+        h-full
+        -top-[120px]
+        xs:-top-[180px]
+        sm:-top-[200px]
+        md:-top-[240px]
+        lg:-top-[280px]
+        xl:-top-[320px]
+        2xl:-top-[440px]
+        select-none
+      `}
+    >
+      <Image
+        className="object-cover sm:object-scale-down"
+        alt=""
+        src={headerGlowSvg as StaticImageData}
+        layout="fill"
+        priority
+        quality={100}
+      />
+    </div>
   </div>
 );
 
