f272a194fb5969963db099bcbdf93ef4036c96a9
Alexander Tesfamichael <alex.tesfamichael@gmail.com>
2022-09-11 20:51:47 +0100

    feat: update merge projection estimate

--- src/eth-constants.ts
+++ src/eth-constants.ts
@@ -4,6 +4,6 @@ export const LONDON_HARDFORK_DATE_TIME = DateFns.parseISO(
   "2021-08-05T00:00:00Z",
 );
 
-export const MERGE_TIMESTAMP_ESTIMATED = new Date("2022-09-16T00:00:00Z");
+export const MERGE_TIMESTAMP_ESTIMATED = new Date("2022-09-15T00:00:00Z");
 
 export const TOTAL_TERMINAL_DIFFICULTY = 5.875e22;
