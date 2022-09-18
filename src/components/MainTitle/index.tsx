448c726e7a939614235a882368180cfbac4e5233
Alexander Tesfamichael <alex.tesfamichael@gmail.com>
2022-09-19 00:36:03 +0100

    feat: style MainTitle

--- src/components/MainTitle/index.tsx
+++ src/components/MainTitle/index.tsx
@@ -1,19 +1,23 @@
 import type { FC, ReactNode } from "react";
 import styles from "./index.module.scss";
 
-const MainTitle: FC<{ children: ReactNode }> = ({ children }) => (
+const MainTitle: FC<{ children: ReactNode; className?: string }> = ({
+  children,
+  className,
+}) => (
   <div
     className={`
       bg-transparent
       font-extralight
       text-white text-center
-      mt-16 mb-8 mx-auto px-4 md:px-16
+      mx-auto px-4 md:px-16
       text-[4.6rem]
       md:text-[4.0rem]
       lg:text-[4.8rem]
       leading-[5.4rem]
       md:leading-[5.4rem]
       ${styles.gradientText}
+      ${className}
     `}
   >
     {children}
