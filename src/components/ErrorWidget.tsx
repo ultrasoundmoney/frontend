31deb59113ae1d509865c0d6ccbc064a12bdd1a2
Alexander Tesfamichael <alex.tesfamichael@gmail.com>
2022-09-18 14:25:19 +0100

    feat: update widget error boundary

--- src/components/ErrorWidget.tsx
+++ src/components/ErrorWidget.tsx
@@ -3,20 +3,17 @@ import type { FC } from "react";
 type Props = { title: string };
 
 const ErrorWidget: FC<Props> = ({ title }) => (
-  <div
-    className={`
-      w-full bg-slateus-700 p-8 rounded-lg
-      border border-red-400
-    `}
-  >
-    <p className="font-inter font-light text-slateus-200 text-xs uppercase tracking-widest mb-4">
+  <div className="w-full p-8 text-base font-light text-white border border-red-400 rounded-lg font-roboto bg-slateus-700">
+    <p className="font-inter font-light text-slateus-200 text-xs uppercase tracking-widest mb-8">
       {title}
     </p>
-    <p className="font-roboto font-light text-white">
-      This component hit an unexpected error, if this does not change soon,
-      please let us know
+    <p className="mb-4">An exception has occured 😱</p>
+    <p className="mb-2">
+      We&apos;re sorry, this should never happen! If you&apos;ve seen this
+      before, or reloading doesn&apos;t fix it, you can help debug it by sending
+      a screenshot to
       <a
-        className="ml-2 font-inter font-light text-slateus-200 active:brightness-90 cursor-pointer"
+        className="ml-2 underline cursor-pointer text-slateus-200 active:brightness-90"
         href="https://twitter.com/ultrasoundmoney/"
         rel="noreferrer"
         target="_blank"
