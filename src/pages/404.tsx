5244410b8b8bd22b66c47f574d0feb45263ae17c
Alexander Tesfamichael <alex.tesfamichael@gmail.com>
2022-09-18 15:39:57 +0100

    feat: custom 404 page

---
+++ src/pages/404.tsx
@@ -0,0 +1,30 @@
+const Custom404 = () => (
+  <div
+    style={{
+      color: "#8991AD",
+      display: "flex",
+      flexDirection: "column",
+      justifyContent: "center",
+      height: "100vh",
+      margin: "0 10vw",
+      alignItems: "center",
+    }}
+  >
+    <h1 style={{ marginBottom: "2rem" }}>404 | no ultra sound money here</h1>
+    <a
+      className="text-slateus-200 active:brightness-90 cursor-pointer"
+      href="https://ultrasound.money/"
+      rel="noreferrer"
+      target="_blank"
+      style={{
+        color: "#B5BDDB",
+        marginLeft: "0.5rem",
+        textDecorationLine: "underline",
+      }}
+    >
+      ultrasound.money
+    </a>
+  </div>
+);
+
+export default Custom404;
