270e3fee34c036303b80dab0cc745096591d4f8e
Alexander Tesfamichael <alex.tesfamichael@gmail.com>
2022-09-21 18:40:24 +0200

    feat: fam page

---
+++ src/components/FamPage/index.tsx
@@ -0,0 +1,72 @@
+import type { FC } from "react";
+import { SkeletonTheme } from "react-loading-skeleton";
+import { useFamCount } from "../../api/fam-count";
+import colors from "../../colors";
+import { useFeatureFlags, FeatureFlagsContext } from "../../feature-flags";
+import { formatZeroDecimals } from "../../format";
+import { useAdminToken } from "../../hooks/use-admin-token";
+import AdminTools from "../AdminTools";
+import BasicErrorBoundary from "../BasicErrorBoundary";
+import JoinDiscordSection from "../Dashboard/JoinDiscordSection";
+import HamburgerMenu from "../HamburgerMenu";
+import HeaderGlow from "../HeaderGlow";
+import MainTitle from "../MainTitle";
+import LabelText from "../TextsNext/LabelText";
+import QuantifyText from "../TextsNext/QuantifyText";
+import PriceGasWidget from "../TopBar/PriceGasWidget";
+import WearTheBatSignal from "../WearTheBatSignal";
+import PoapSection from "./PoapSection";
+
+const FamPage: FC = () => {
+  const famCount = useFamCount();
+  const adminToken = useAdminToken();
+  const { featureFlags, setFlag } = useFeatureFlags();
+
+  return (
+    <FeatureFlagsContext.Provider value={featureFlags}>
+      <SkeletonTheme
+        baseColor={colors.slateus500}
+        highlightColor={"#565b7f"}
+        enableAnimation={true}
+      >
+        <div className="container mx-auto md:px-16 mb-16">
+          {adminToken && (
+            <BasicErrorBoundary>
+              <AdminTools setFlag={setFlag} />
+            </BasicErrorBoundary>
+          )}
+          <HeaderGlow />
+          <div className="flex p-4 items-center justify-between z-10 w-full">
+            <PriceGasWidget />
+            <HamburgerMenu />
+          </div>
+          <MainTitle className="mt-8">ultra sound fam</MainTitle>
+          <BasicErrorBoundary>
+            <section className="flex flex-col gap-y-4 mt-16">
+              <div className="bg-slateus-700 p-8 flex flex-col gap-y-4 rounded-lg">
+                <LabelText>fam count</LabelText>
+                <div className="flex gap-x-1">
+                  <QuantifyText className="text-3xl">
+                    {formatZeroDecimals(famCount.count)}
+                  </QuantifyText>
+                  <QuantifyText className="text-3xl text-slateus-400 ml-2">
+                    members
+                  </QuantifyText>
+                </div>
+              </div>
+              <WearTheBatSignal />
+            </section>
+          </BasicErrorBoundary>
+          <BasicErrorBoundary>
+            <PoapSection />
+          </BasicErrorBoundary>
+          <BasicErrorBoundary>
+            <JoinDiscordSection />
+          </BasicErrorBoundary>
+        </div>
+      </SkeletonTheme>
+    </FeatureFlagsContext.Provider>
+  );
+};
+
+export default FamPage;
---
+++ src/components/StatusText.tsx
@@ -0,0 +1,19 @@
+import type { FC, ReactNode } from "react";
+import BodyTextV2 from "./TextsNext/BodyTextV2";
+
+export const LoadingText: FC<{ children: ReactNode }> = ({ children }) => (
+  <BodyTextV2 className="text-white animate-pulse">{children}</BodyTextV2>
+);
+
+export const PositiveText: FC<{ children: ReactNode }> = ({ children }) => (
+  <BodyTextV2 className="text-green-400">{children}</BodyTextV2>
+);
+
+export const NegativeText: FC<{ children: ReactNode }> = ({ children }) => (
+  <BodyTextV2 className="whitespace-nowrap text-red-400">{children}</BodyTextV2>
+);
+
+// Dummy item to fix baseline alignment between sections on md
+export const AlignmentText: FC = () => (
+  <BodyTextV2 className="select-none">&nbsp;</BodyTextV2>
+);
