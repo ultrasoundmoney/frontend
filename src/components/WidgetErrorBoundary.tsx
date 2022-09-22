37000801064bde05c4f703fe0af3ce74e6656008
Alexander Tesfamichael <alex.tesfamichael@gmail.com>
2022-09-22 15:13:16 +0200

    refactor: make WidgetErrorBoundary title required

--- src/components/WidgetErrorBoundary.tsx
+++ src/components/WidgetErrorBoundary.tsx
@@ -2,10 +2,9 @@ import * as Sentry from "@sentry/nextjs";
 import type { FC, ReactNode } from "react";
 import ErrorWidget from "./ErrorWidget";
 
-const WidgetErrorBoundary: FC<{ children: ReactNode; title?: string }> = ({
-  children,
-  title = "unexpected error",
-}) => (
+type Props = { children: ReactNode; title: string };
+
+const WidgetErrorBoundary: FC<Props> = ({ children, title }) => (
   <Sentry.ErrorBoundary fallback={<ErrorWidget title={title} />}>
     {children}
   </Sentry.ErrorBoundary>
