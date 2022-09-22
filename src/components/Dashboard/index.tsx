8940327014829a5612f6035a93210392222446e8
Alexander Tesfamichael <alex.tesfamichael@gmail.com>
2022-09-22 14:45:50 +0200

    refactor: extract section into module

--- src/components/Dashboard/index.tsx
+++ src/components/Dashboard/index.tsx
@@ -22,6 +22,7 @@ import confettiSvg from "../../assets/confetti-own.svg";
 import pandaSvg from "../../assets/panda-own.svg";
 import type { StaticImageData } from "next/image";
 import Image from "next/image";
+import ContactSection from "./ContactSection";
 
 const AdminTools = dynamic(() => import("../AdminTools"), { ssr: false });
 // We get hydration errors in production.
@@ -155,39 +156,7 @@ const Dashboard: FC = () => {
               <FaqBlock />
             </div>
           </div>
-          <div className="w-full flex flex-col items-center pb-40">
-            <SectionDivider title="still have questions?" />
-            <div className="flex flex-col gap-y-4 justify-start">
-              <div className="flex gap-2 items-center">
-                <img
-                  className="w-4"
-                  src="/twitter-icon.svg"
-                  alt="icon of the twitter bird"
-                />
-                <StyledLink
-                  className="flex items-center gap-x-2"
-                  enableHover={false}
-                  href="https://twitter.com/ultrasoundmoney/"
-                >
-                  <TextInterLink>DM us @ultrasoundmoney</TextInterLink>
-                </StyledLink>
-              </div>
-              <div className="flex gap-2 items-center">
-                <img
-                  className="h-4"
-                  src="/email-icon.svg"
-                  alt="icon of an envelope, email"
-                />
-                <StyledLink
-                  className="flex items-center gap-x-2"
-                  enableHover={false}
-                  href="mailto:contact@ultrasound.money"
-                >
-                  <TextInterLink>contact@ultrasound.money</TextInterLink>
-                </StyledLink>
-              </div>
-            </div>
-          </div>
+          <ContactSection />
         </div>
       </SkeletonTheme>
     </FeatureFlagsContext.Provider>
