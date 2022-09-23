d07b240039b16454d4475d1f6b6961cc97c4249b
Alexander Tesfamichael <alex.tesfamichael@gmail.com>
2022-09-23 15:33:05 +0200

    feat: scroll to auth from section
    Scroll the view to the section the user was authenticating from if such
    state exists.

--- src/components/Dashboard/JoinDiscordSection.tsx
+++ src/components/Dashboard/JoinDiscordSection.tsx
@@ -1,18 +1,24 @@
 import type { FC } from "react";
+import type { AuthFromSection } from "../../hooks/use-auth-from-section";
 import JoinDiscordWidget from "../JoinDiscordWidget";
 import { SectionTitle } from "../TextsNext/SectionTitle";
 
-const JoinDiscordSection: FC = () => (
-  <section className="mt-16 px-4 md:px-16" id="discord">
-    <SectionTitle
-      className="py-16"
-      link="discord"
-      highlightGradient="bg-gradient-to-br from-indigo-400 to-indigo-700"
-      highlightPhrase="Discord"
-    >
-      ultra sound
-    </SectionTitle>
-    <JoinDiscordWidget />
-  </section>
-);
+const JoinDiscordSection: FC = () => {
+  const id: AuthFromSection = "discord";
+
+  return (
+    <section className="mt-16 px-4 md:px-16" id={id}>
+      <SectionTitle
+        className="py-16"
+        link="discord"
+        highlightGradient="bg-gradient-to-br from-indigo-400 to-indigo-700"
+        highlightPhrase="Discord"
+      >
+        ultra sound
+      </SectionTitle>
+      <JoinDiscordWidget />
+    </section>
+  );
+};
+
 export default JoinDiscordSection;
