80574336e3f8b557afd0a2bc57367e5e052fc402
Alexander Tesfamichael <alex.tesfamichael@gmail.com>
2022-10-07 17:29:26 -0500

    style: sort classnames

--- src/components/TwitterFam/index.tsx
+++ src/components/TwitterFam/index.tsx
@@ -142,14 +142,14 @@ const TwitterFam: FC = () => {
 
   return (
     <>
-      <h1 className="text-white font-light text-center text-2xl md:text-3xl xl:text-41xl mb-8">
+      <h1 className="mb-8 text-center text-2xl font-light text-white md:text-3xl xl:text-41xl">
         <a
           target="_blank"
           href="https://twitter.com/i/lists/1376636817089396750/members"
           rel="noopener noreferrer"
           role="link"
           title="join the ultra sound Twitter fam"
-          className="hover:underline hover:text-blue-spindle relative h-full"
+          className="relative h-full hover:text-blue-spindle hover:underline"
         >
           join 5000+ fam members
         </a>
@@ -158,19 +158,19 @@ const TwitterFam: FC = () => {
         <p className="text-blue-shipcove md:text-lg">wear the bat signal</p>
         <div className="w-4"></div>
         <CopyToClipboard text={"🦇🔊"} onCopy={onBatSoundCopied}>
-          <span className="relative bg-blue-midnightexpress border border-gray-700 rounded-full p-2 pl-5 flex w-48 mx-auto justify-between items-center text-2xl isolate clipboard-emoji cursor-pointer">
+          <span className="clipboard-emoji relative isolate mx-auto flex w-48 cursor-pointer items-center justify-between rounded-full border border-gray-700 bg-blue-midnightexpress p-2 pl-5 text-2xl">
             <Twemoji className="flex gap-x-1" imageClassName="w-7" wrapper>
               🦇🔊
             </Twemoji>
-            <span className="font-light text-base copy-container rounded-full bg-mediumspring text-blue-midnightexpress px-5 py-1 isolate select-none">
+            <span className="copy-container isolate select-none rounded-full bg-mediumspring px-5 py-1 text-base font-light text-blue-midnightexpress">
               copy
             </span>
             <span
-              className={`absolute left-0 right-0 top-0 bottom-0 p-1 bg-blue-midnightexpress flex justify-center items-center rounded-full transition-opacity ${
+              className={`absolute left-0 right-0 top-0 bottom-0 flex items-center justify-center rounded-full bg-blue-midnightexpress p-1 transition-opacity ${
                 isCopiedFeedbackVisible ? "opacity-100" : "opacity-0"
               }`}
             >
-              <p className="font-inter font-light text-base text-white">
+              <p className="font-inter text-base font-light text-white">
                 copied!
               </p>
             </span>
@@ -182,7 +182,7 @@ const TwitterFam: FC = () => {
         {currentProfiles.map((profile, index) => (
           <ImageWithTooltip
             key={profile?.profileUrl ?? index}
-            className="m-2 w-10 h-10 select-none"
+            className="m-2 h-10 w-10 select-none"
             imageUrl={profile?.profileImageUrl}
             isDoneLoading={profile !== undefined}
             skeletonDiameter="40px"
@@ -203,7 +203,7 @@ const TwitterFam: FC = () => {
       <>
         <div
           ref={setPopperEl}
-          className="z-10 hidden md:block p-4"
+          className="z-10 hidden p-4 md:block"
           style={{
             ...popperStyles.popper,
             visibility: showTooltip && md ? "visible" : "hidden",
