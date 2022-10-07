3a426d40909966a45385cf562998466aace0fdeb
Alexander Tesfamichael <alex.tesfamichael@gmail.com>
2022-10-07 18:23:09 -0500

    feat: don't ssr fam section widgets

--- src/components/Dashboard/FamSection.tsx
+++ src/components/Dashboard/FamSection.tsx
@@ -1,19 +1,19 @@
 import dynamic from "next/dynamic";
 import type { FC } from "react";
 import BasicErrorBoundary from "../BasicErrorBoundary";
-const TwitterFam = dynamic(() => import("../TwitterFam"));
-const FollowingYou = dynamic(() => import("../FollowingYou"));
+const TwitterFam = dynamic(() => import("../TwitterFam"), { ssr: false });
+const FollowingYou = dynamic(() => import("../FollowingYou"), { ssr: false });
 
 const FamSection: FC = () => (
   <BasicErrorBoundary>
     <div className="flex flex-col xs:px-4 md:px-16">
-      <div id="fam" className="relative flex px-4 md:px-0 pt-40 mb-16">
-        <div className="w-full relative flex flex-col items-center">
+      <div id="fam" className="relative mb-16 flex px-4 pt-40 md:px-0">
+        <div className="relative flex w-full flex-col items-center">
           <TwitterFam />
         </div>
       </div>
-      <div className="flex px-4 md:px-0 pt-20">
-        <div className="w-full lg:w-2/3 md:m-auto relative">
+      <div className="flex px-4 pt-20 md:px-0">
+        <div className="relative w-full md:m-auto lg:w-2/3">
           <FollowingYou />
         </div>
       </div>
