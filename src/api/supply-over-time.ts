b294e74e505dd702fc8c523302fc83296bcbfbf8
Alexander Tesfamichael <alex.tesfamichael@gmail.com>
2022-10-31 13:40:16 -0600

    feat: filter SupplyOverTime for post-merge

--- src/api/supply-over-time.ts
+++ src/api/supply-over-time.ts
@@ -1,9 +1,9 @@
-import { secondsToMilliseconds } from "date-fns";
+import { isBefore, secondsToMilliseconds } from "date-fns";
 import useSWR from "swr";
 import type { Slot } from "../beacon-units";
 import type { EthNumber } from "../eth-units";
+import { PARIS_TIMESTAMP } from "../hardforks/paris";
 import type { DateTimeString } from "../time";
-import type { EthPriceStats } from "./eth-price-stats";
 import type { ApiResult } from "./fetchers";
 import { fetchJson, fetchJsonSwr } from "./fetchers";
 
@@ -14,8 +14,6 @@ export type SupplyAtTime = {
 };
 
 export type BlockNumber = number;
-
-export type TimeFrameV2Api = "m5" | "h1" | "d1" | "d7" | "d30" | "since_merge";
 
 export type SupplyOverTime = {
   block_number: BlockNumber;
@@ -31,13 +29,44 @@ export type SupplyOverTime = {
 
 const url = "/api/v2/fees/supply-over-time";
 
-export const fetchSupplyOverTime = (): Promise<ApiResult<EthPriceStats>> =>
-  fetchJson<EthPriceStats>(url);
+// To calculate rates based on points we look at the first and last point in a time frame. This works for all but the since-merge time frame as it currently includes pre-merge points we don't want to include in the rate calculation. To rectify this we slice them out until the backend stops sending them.
+// TODO: deploy this code to prod so the frontend has correct behavior when receiving only post-merge points.
+// TODO: have the backend stop sending pre-merge points for the since-merge time frame.
+// TODO: remove the pre-merge point filtering code.
+const filterPostParisPoints = (points: SupplyAtTime[]): SupplyAtTime[] => {
+  const mergeIndex = points.findIndex(
+    // !isBefore meaning at or after Paris
+    ({ timestamp }) => !isBefore(new Date(timestamp), PARIS_TIMESTAMP),
+  );
+  return points.slice(mergeIndex);
+};
+
+export const fetchSupplyOverTime = (): Promise<ApiResult<SupplyOverTime>> =>
+  fetchJson<SupplyOverTime>(url).then((res) => {
+    if ("error" in res) {
+      return res;
+    }
+
+    return {
+      ...res,
+      data: {
+        ...res.data,
+        since_merge: filterPostParisPoints(res.data.since_merge),
+      },
+    };
+  });
 
 export const useSupplyOverTime = (): SupplyOverTime | undefined => {
   const { data } = useSWR<SupplyOverTime>(url, fetchJsonSwr, {
     refreshInterval: secondsToMilliseconds(4),
   });
 
-  return data;
+  if (data === undefined) {
+    return undefined;
+  }
+
+  return {
+    ...data,
+    since_merge: filterPostParisPoints(data.since_merge),
+  };
 };
