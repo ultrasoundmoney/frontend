bb531032c552d6e26e860c56a6bd0219e6bb5317
Alexander Tesfamichael <alex.tesfamichael@gmail.com>
2022-11-02 14:20:03 -0600

    feat(colors): sort and mark deprecated

--- src/colors.ts
+++ src/colors.ts
@@ -1,19 +1,24 @@
 const colors = {
+  blue400: "#60a5fa",
   cloudyblue: "#aaaad2",
+  /** @deprecated use blue400 instead. */
   drop: "#5dadec",
-  dusk: "#464b6f",
-  slateus500: "#464b6f",
   fireHighlight: "#ffcc4d",
+  /** @deprecated use orange400 instead. */
   fireOrange: "#f4900c",
-  slateus450: "#8888af",
-  slateus800: "#131827",
-  slateus400: "#8991ad",
+  orange400: "#fb923c",
+  slateus100: "#dee2f1",
   /** @deprecated use slateus200 instead. */
   spindle: "#b5bddb",
   slateus200: "#b5bddb",
-  slateus700: "#1b2236",
+  slateus400: "#8991ad",
+  slateus450: "#8888af",
+  /** @deprecated use slateus500 instead. */
+  dusk: "#464b6f",
+  slateus500: "#464b6f",
+  slateus800: "#131827",
   slateus600: "#2d344a",
-  slateus100: "#dee2f1",
+  slateus700: "#1b2236",
   white: "#ffffff",
 };
 
