e509562aee30ebf6b20131669cd62efdff37a826
Alexander Tesfamichael <alex.tesfamichael@gmail.com>
2022-11-02 14:25:56 -0600

    feat(api): don't capture fetch error
    Network failures happen a lot. Our dashboard makes about a request per
    second, so inevitably some fail. We make network errors expected,
    letting them bubble up, expecting callers to deal with them.

--- src/api/fetchers.ts
+++ src/api/fetchers.ts
@@ -1,4 +1,3 @@
-import { captureException } from "@sentry/nextjs";
 import { getDomain } from "../config";
 
 export type ApiResult<A> = { data: A } | { error: Error };
@@ -51,22 +50,20 @@ const fetchUnsafe = async <A>(url: RequestInfo): Promise<ApiResult<A>> => {
 
 export const fetchJson = async <A>(url: RequestInfo): Promise<ApiResult<A>> => {
   try {
-    return await fetchUnsafe(url);
+    const res = await fetchUnsafe<A>(url);
+    return res;
   } catch (error) {
     if (error instanceof Error) {
-      captureException(error);
       return { error };
     }
 
     if (typeof error === "string") {
-      captureException(error);
       return { error: new Error(error) };
     }
 
     const errorObj = new Error(
       `fetch failed, caught something, but not an error, ${error}`,
     );
-    captureException(errorObj);
     return {
       error: errorObj,
     };
