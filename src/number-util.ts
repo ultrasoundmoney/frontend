cdf3726731d851bb70f8f35bdfc90b90c2e2de5c
Grzegorz Kućmierz <gkucmierz@gmail.com>
2022-11-09 00:54:18 +0100

    refactor: use built-in round method

--- src/number-util.ts
+++ src/number-util.ts
@@ -1,4 +1,1 @@
-export const round = (value: number, precision = 0) => {
-  const multiplier = 10 ** precision;
-  return Math.round(value * multiplier) / multiplier;
-};
+export const round = (value: number, precision = 0) => +value.toFixed(precision);
