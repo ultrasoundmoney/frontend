44871ee85f84166cae31700cb90f1a790f7a84d0
Alexander Tesfamichael <alex.tesfamichael@gmail.com>
2022-11-29 12:51:01 -0600

    feat(sentry): higher sample rate everywhere
    Uses sample rate of 1, everywhere but prod.

--- sentry.client.config.ts
+++ sentry.client.config.ts
@@ -4,5 +4,5 @@ Sentry.init({
   dsn: "https://3ef07f75a826463a94510b8d676fd7e3@o920717.ingest.sentry.io/5866579",
   enabled: process.env.NEXT_PUBLIC_ENV !== "dev",
   environment: process.env.NEXT_PUBLIC_ENV,
-  tracesSampleRate: process.env.NEXT_PUBLIC_ENV === "stag" ? 1 : 0.1,
+  tracesSampleRate: process.env.NEXT_PUBLIC_ENV === "prod" ? 0.1 : 1,
 });
