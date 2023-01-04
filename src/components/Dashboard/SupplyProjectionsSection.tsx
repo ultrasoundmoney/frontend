e189507d728fa2d87372fdaf4633be773830ba8b
Alexander Tesfamichael <alex.tesfamichael@gmail.com>
2023-01-04 18:49:22 +0700

    feat: reword projections section

--- src/components/Dashboard/SupplyProjectionsSection.tsx
+++ src/components/Dashboard/SupplyProjectionsSection.tsx
@@ -8,9 +8,9 @@ const SupplyProjectionsSection: FC = () => (
   <BasicErrorBoundary>
     <div id="projection" className="mt-16">
       <SectionDivider
-        link="projection"
+        link="projections"
         subtitle="ultra sound for decades to come"
-        title="supply projections"
+        title="projections"
       />
       <div className="flex flex-col gap-4 xs:px-4 md:px-16">
         <EquilibriumWidget />
