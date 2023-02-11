b80dae380ebf6b03fca3bf6c9b5644b7d41456d7
Paul Wackerow <54227730+wackerow@users.noreply.github.com>
2023-02-11 11:15:19 -0800

    Revert "fix: overflow-hidden for bg gradients [Fixes #266]"
    This reverts commit 354baf84add44024f8405ae55caad53fb06d0bec.

--- src/components/EthSupplyWidget/index.tsx
+++ src/components/EthSupplyWidget/index.tsx
@@ -642,8 +642,8 @@ const SupplySinceMergeWidget: FC<Props> = ({
 
   return (
     <WidgetErrorBoundary title="eth supply">
-      <WidgetBackground className="relative flex w-full flex-col overflow-hidden">
-        <div className="pointer-events-none absolute left-0 right-0 top-0 bottom-0">
+      <WidgetBackground className="relative flex w-full flex-col">
+        <div className="pointer-events-none absolute left-0 right-0 top-0 bottom-0 overflow-hidden">
           <div
             // will-change-transform is critical for mobile performance of
             // rendering the chart overlayed on this element.
