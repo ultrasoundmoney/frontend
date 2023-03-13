154fd1abc9d4befe8d69351f1a79c82c0953e128
Alexander Tesfamichael <alex.tesfamichael@gmail.com>
2023-03-13 15:19:14 +0100

    refactor: sort classes

--- src/relay/sections/InclusionDelaySection/index.tsx
+++ src/relay/sections/InclusionDelaySection/index.tsx
@@ -36,8 +36,8 @@ const InclusionDelaySection: FC<Props> = ({
       subtitle="it can take time"
       link="inclusion-delay"
     >
-      <div className="flex w-full justify-center rounded-lg bg-slateus-700 p-8">
-        <div className="flex items-center gap-4">
+      <div className="flex justify-center p-8 w-full rounded-lg bg-slateus-700">
+        <div className="flex gap-4 items-center">
           <LabelText>time frame</LabelText>
           <TimeFrameControl
             selectedTimeFrame={timeFrame}
@@ -48,7 +48,7 @@ const InclusionDelaySection: FC<Props> = ({
           />
         </div>
       </div>
-      <div className="grid w-full grid-cols-1 gap-4 lg:grid-cols-2">
+      <div className="grid grid-cols-1 gap-4 w-full lg:grid-cols-2">
         <InclusionTimesWidget
           inclusionTimes={inclusionTimes}
           timeFrame={timeFrame}
