a2d90a5ec73ac721e6cb665b5de0d4a66c0230ee
kylekaplan <kylekaplan50@gmail.com>
2023-03-17 10:46:54 -0400

    update default zoom level

--- src/mainsite/components/TwitterFam/index.tsx
+++ src/mainsite/components/TwitterFam/index.tsx
@@ -250,7 +250,7 @@ const TwitterFam: FC = () => {
       <BasicErrorBoundary>
         <TransformWrapper
           ref={panZoomRef}
-          initialScale={3}
+          initialScale={4.5}
           initialPositionX={0}
           initialPositionY={0}
           wheel={{ wheelDisabled: true }}
@@ -335,7 +335,7 @@ const TwitterFam: FC = () => {
                             event.preventDefault();
                             const el = filteredProfiles?.[currentProfileShow]?.handle.toLowerCase()
                             if (el) {
-                              zoomToElement(document.getElementById(el) || '', 3);
+                              zoomToElement(document.getElementById(el) || '', 4.5);
                               setCurrentProfileShow((prev: number) => {
                                 if (prev === filteredProfilesCount - 1) {
                                   return 0;
