675eff6c8f6a91d4c93491d06f85ecee9ee8b17f
ckoopmann <c.k.e.koopmann@gmail.com>
2023-03-20 18:17:21 +0800

    Remove hack for base-fee sinc_merge data

--- src/mainsite/api/base-fee-over-time.ts
+++ src/mainsite/api/base-fee-over-time.ts
@@ -1,11 +1,10 @@
-import { isAfter, secondsToMilliseconds } from "date-fns";
+import { secondsToMilliseconds } from "date-fns";
 import useSWR from "swr";
 import type { Gwei, WeiNumber } from "../../eth-units";
 import type { DateTimeString } from "../../time";
 import type { ApiResult } from "../../fetchers";
 import { fetchApiJson } from "./fetchers";
 import { fetchJsonSwr } from "./fetchers";
-import { MERGE_TIMESTAMP } from "../hardforks/paris";
 
 export type BaseFeeAtTime = {
   block_number: number | null;
@@ -30,19 +29,10 @@ const url = "/api/v2/fees/base-fee-over-time";
 export const fetchBaseFeeOverTime = (): Promise<ApiResult<BaseFeeOverTime>> =>
   fetchApiJson<BaseFeeOverTime>(url);
 
-const filterDaysSinceMerge = (input: BaseFeeAtTime[]): BaseFeeAtTime[] => {
-  return input.filter((item) => {
-    return isAfter(Date.parse(item.timestamp), MERGE_TIMESTAMP);
-  });
-};
-
 export const useBaseFeeOverTime = (): BaseFeeOverTime | undefined => {
   const { data } = useSWR<BaseFeeOverTime>(url, fetchJsonSwr, {
     refreshInterval: secondsToMilliseconds(4),
   });
-  if (data != null && data.since_merge === null) {
-    data.since_merge = filterDaysSinceMerge(data.since_burn);
-  }
 
   return data;
 };
