685b348babbaeec85fe00052bc319d7e30fc65c0
christn <c.k.e.koopmann@gmail.com>
2023-12-11 12:51:07 +0800

    Add withPWA to config

--- next.config.js
+++ next.config.js
@@ -2,65 +2,70 @@ const { withSentryConfig } = require("@sentry/nextjs");
 const withBundleAnalyzer = require("@next/bundle-analyzer")({
   enabled: process.env.ANALYZE_BUNDLE === "true",
 });
+const withPWA = require("next-pwa")({
+  dest: "public",
+});
 
 /** @type {import('next').NextConfig} */
-const nextConfig = withSentryConfig(
-  withBundleAnalyzer({
-    productionBrowserSourceMaps: true,
-    sentry: {
-      disableServerWebpackPlugin:
-        process.env.NEXT_PUBLIC_ENV !== "stag" &&
-        process.env.NEXT_PUBLIC_ENV !== "prod",
-      disableClientWebpackPlugin:
-        process.env.NEXT_PUBLIC_ENV !== "stag" &&
-        process.env.NEXT_PUBLIC_ENV !== "prod",
-    },
-    staticPageGenerationTimeout: 60 * 4,
-    output: "standalone",
-    reactStrictMode: true,
-    swcMinify: true,
-    images: {
-      domains: [
-        "abs.twimg.com",
-        "assets.coingecko.com",
-        "i.seadn.io",
-        "lh3.googleusercontent.com",
-        "openseauserdata.com",
-        "pbs.twimg.com",
-      ],
-    },
-    rewrites: async () => [
-      {
-        source: "/api/:path*",
-        has: [{ type: "host", value: "localhost" }],
-        destination:
-          process.env["NEXT_PUBLIC_ENV"] === "stag"
-            ? "https://usm-i7x0.ultrasound.money/api/:path*"
-            : "https://ultrasound.money/api/:path*",
+const nextConfig = withPWA(
+  withSentryConfig(
+    withBundleAnalyzer({
+      productionBrowserSourceMaps: true,
+      sentry: {
+        disableServerWebpackPlugin:
+          process.env.NEXT_PUBLIC_ENV !== "stag" &&
+          process.env.NEXT_PUBLIC_ENV !== "prod",
+        disableClientWebpackPlugin:
+          process.env.NEXT_PUBLIC_ENV !== "stag" &&
+          process.env.NEXT_PUBLIC_ENV !== "prod",
       },
-      // To test locally with the fam-analysis api running and a local tunnel to receive the oauth2 callback, use the below rewrite.
-      // {
-      //   source: "/api/auth/:path*",
-      //   has: [
-      //     {
-      //       type: "host",
-      //       value: "oyfy.tunnelto.dev",
-      //     },
-      //   ],
-      //   destination: "http://127.0.0.1:3001/api/auth/:path*",
-      // },
-      // {
-      //   source: "/api/fam/queue-for-discord",
-      //   has: [
-      //     {
-      //       type: "host",
-      //       value: "oyfy.tunnelto.dev",
-      //     },
-      //   ],
-      //   destination: "http://127.0.0.1:3001/api/fam/queue-for-discord",
-      // },
-    ],
-  }),
+      staticPageGenerationTimeout: 60 * 4,
+      output: "standalone",
+      reactStrictMode: true,
+      swcMinify: true,
+      images: {
+        domains: [
+          "abs.twimg.com",
+          "assets.coingecko.com",
+          "i.seadn.io",
+          "lh3.googleusercontent.com",
+          "openseauserdata.com",
+          "pbs.twimg.com",
+        ],
+      },
+      rewrites: async () => [
+        {
+          source: "/api/:path*",
+          has: [{ type: "host", value: "localhost" }],
+          destination:
+            process.env["NEXT_PUBLIC_ENV"] === "stag"
+              ? "https://usm-i7x0.ultrasound.money/api/:path*"
+              : "https://ultrasound.money/api/:path*",
+        },
+        // To test locally with the fam-analysis api running and a local tunnel to receive the oauth2 callback, use the below rewrite.
+        // {
+        //   source: "/api/auth/:path*",
+        //   has: [
+        //     {
+        //       type: "host",
+        //       value: "oyfy.tunnelto.dev",
+        //     },
+        //   ],
+        //   destination: "http://127.0.0.1:3001/api/auth/:path*",
+        // },
+        // {
+        //   source: "/api/fam/queue-for-discord",
+        //   has: [
+        //     {
+        //       type: "host",
+        //       value: "oyfy.tunnelto.dev",
+        //     },
+        //   ],
+        //   destination: "http://127.0.0.1:3001/api/fam/queue-for-discord",
+        // },
+      ],
+    }),
+  ),
 );
 
 module.exports = nextConfig;
