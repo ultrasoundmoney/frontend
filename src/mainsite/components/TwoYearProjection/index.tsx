4b496b14e7ade5740a183c5ff17209b702a5e7f8
christn <c.k.e.koopmann@gmail.com>
2024-02-07 14:31:17 +0800

    Set eth amount staked dynamically in two year projection

--- src/mainsite/components/TwoYearProjection/index.tsx
+++ src/mainsite/components/TwoYearProjection/index.tsx
@@ -1,6 +1,7 @@
 import dynamic from "next/dynamic";
 import TranslationsContext from "../../contexts/TranslationsContext";
 import { formatOneDecimal } from "../../../format";
+import { GWEI_PER_ETH, Gwei } from "../../../eth-units";
 import {
   estimatedDailyFeeBurn,
   estimatedDailyIssuance,
@@ -10,7 +11,8 @@ import { BaseText } from "../../../components/Texts";
 import Twemoji from "../../../components/Twemoji";
 import styles from "./TwoYearProjection.module.scss";
 import type { ChangeEvent, FC, ReactNode } from "react";
-import { useCallback, useState, useContext } from "react";
+import { useCallback, useEffect, useState, useContext } from "react";
+import { useEffectiveBalanceSum } from "../../api/effective-balance-sum";
 const SupplyChart = dynamic(() => import("./TwoYearProjectionChart"));
 
 const MIN_PROJECTED_ETH_STAKING = 1e6;
@@ -64,12 +66,54 @@ const TwoYearProjection: FC = () => {
     setIsPeakPresent(isPeakPresent);
   }, []);
 
+  const [stakingAprFraction, setStakingAprFraction] = useState<number>(0);
+  const BASE_REWARD_FACTOR = 64;
+  const SECONDS_PER_SLOT = 12;
+  const SLOTS_PER_EPOCH = 32;
+  const EPOCHS_PER_DAY: number =
+    (24 * 60 * 60) / SLOTS_PER_EPOCH / SECONDS_PER_SLOT;
+  const EPOCHS_PER_YEAR: number = 365.25 * EPOCHS_PER_DAY;
+  const MAX_EFFECTIVE_BALANCE: number = 32 * GWEI_PER_ETH;
+  const BASE_REWARDS_PER_EPOCH = 4;
+
+  const getIssuanceApr = (effective_balance_sum: Gwei): number => {
+    const balance_sum_gwei = effective_balance_sum;
+    const max_issuance_per_epoch = Math.trunc(
+      (BASE_REWARD_FACTOR * balance_sum_gwei) /
+        Math.floor(Math.sqrt(balance_sum_gwei)),
+    );
+    const issuancePerYear = max_issuance_per_epoch * EPOCHS_PER_YEAR;
+    const apr = issuancePerYear / balance_sum_gwei;
+    return apr;
+  };
+
+  const getStakedFromApr = (apr: number): number => {
+    const baseReward = (apr * 32e9) / 4 / EPOCHS_PER_YEAR;
+    const active_validators =
+      ((MAX_EFFECTIVE_BALANCE * BASE_REWARD_FACTOR) /
+        BASE_REWARDS_PER_EPOCH /
+        baseReward) **
+        2 /
+      32e9;
+    return active_validators * 32;
+  };
+
+  const effectiveBalanceSum = useEffectiveBalanceSum();
+
+  useEffect(() => {
+    if (effectiveBalanceSum === undefined) {
+      return;
+    }
+    const initialStakingApr = getIssuanceApr(effectiveBalanceSum.sum);
+    setStakingAprFraction(initialStakingApr);
+  }, [effectiveBalanceSum, stakingAprFraction]);
+
   return (
     <>
       <div className={styles.chartHeader}>
         <BaseText
           font="font-inter"
-          className="flex items-center px-3 pb-8 text-xs tracking-widest uppercase text-slateus-200"
+          className="flex items-center px-3 pb-8 text-xs uppercase tracking-widest text-slateus-200"
           inline
         >
           ETH supply—2y projection
@@ -97,7 +141,7 @@ const TwoYearProjection: FC = () => {
           title={t.eth_staked}
           value={
             <>
-              {projectedStaking / 1e6}
+              {getStakedFromApr(stakingAprFraction) / 1e6}
               {t.numeric_million_abbrev} ETH
             </>
           }
