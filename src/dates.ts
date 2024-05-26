73c316e37b58c61bd8a343f21b6ba435e0e30177
christn <c.k.e.koopmann@gmail.com>
2024-05-26 14:51:12 +0800

    Fix ico date

--- src/dates.ts
+++ src/dates.ts
@@ -1,6 +1,6 @@
 import * as DateFns from "date-fns";
 
-export const ico = DateFns.parseISO("2021-09-02T00:00:00.000Z");
+export const ico = DateFns.parseISO("2014-09-02T00:00:00.000Z");
 
 export const londonHardFork = DateFns.parseISO("2021-08-05T12:33:42.000Z");
 
