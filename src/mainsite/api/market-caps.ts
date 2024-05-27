0611ab19e83abd291b752c69f3329f3ebf9527ed
christn <c.k.e.koopmann@gmail.com>
2024-05-27 17:48:45 +0800

    Undo change in useMarketCaps

--- src/mainsite/api/market-caps.ts
+++ src/mainsite/api/market-caps.ts
@@ -1,7 +1,6 @@
 import useSWR from "swr";
 import * as Duration from "../../duration";
 import { fetchJsonSwr } from "./fetchers";
-import { useFlippeningData } from "./flippening-data";
 
 type MarketCaps = {
   btcMarketCap: number;
@@ -15,19 +14,6 @@ export const useMarketCaps = (): MarketCaps | undefined => {
   const { data } = useSWR<MarketCaps>("/api/fees/market-caps", fetchJsonSwr, {
     refreshInterval: Duration.millisFromSeconds(30),
   });
-  const flippeningData = useFlippeningData();
-  const lastFlippeningDatapoint = flippeningData?.[flippeningData.length - 1];
 
-  const marketCapData =
-    (data  === undefined || lastFlippeningDatapoint === undefined)
-      ? undefined
-      : {
-          btcMarketCap: lastFlippeningDatapoint.btcMarketcap,
-          ethMarketCap: lastFlippeningDatapoint.ethMarketcap,
-          goldMarketCap: data.goldMarketCap,
-          usdM3MarketCap: data.usdM3MarketCap,
-          timestamp: data.timestamp,
-        };
-
-  return marketCapData;
+  return data;
 };
