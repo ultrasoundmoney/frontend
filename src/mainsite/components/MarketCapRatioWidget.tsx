cc12d69400bde95974966f19c99403e41ebcb2f2
christn <c.k.e.koopmann@gmail.com>
2024-05-27 12:42:28 +0800

    Fixed width table in tooltip

--- src/mainsite/components/MarketCapRatioWidget.tsx
+++ src/mainsite/components/MarketCapRatioWidget.tsx
@@ -375,19 +375,19 @@ const MarketCapRatiosWidget: FC<Props> = ({
             rows = [
               `<tr>
                 <td>
-                    <div class="tt-series">
+                    <div class="tt-series text-right">
                     <div class="tt-series-name text-slate-300">flippening</div>
                     </div>
                 </td>
-                <td class="text-white">${formatOneDecimal(p.y || 0)}%</td>
+                <td class="text-white text-right">${formatOneDecimal(p.y || 0)}%</td>
               </tr>`,
-              `<tr>
+              `<tr w-full>
                 <td>
                     <div class="tt-series">
                     <div class="tt-series-name text-slate-300">outperformance</div>
                     </div>
                 </td>
-                <td class="text-white">${formatOneDecimal(
+                <td class="text-white text-right">${formatOneDecimal(
                   outPerformanceTodate,
                 )}x</td>
               </tr>`,
@@ -397,17 +397,17 @@ const MarketCapRatiosWidget: FC<Props> = ({
                     <div class="tt-series-name text-slate-300">outperformance left</div>
                     </div>
                 </td>
-                <td class="text-white">${formatOneDecimal(
+                <td class="text-white text-right">${formatOneDecimal(
                   remainingOutPerformance,
                 )}x</td>
               </tr>`,
             ];
           }
 
-          const table = `<table><tbody>${rows.join("")}</tbody></table>`;
+          const table = `<table class="tooltip-table"><tbody>${rows.join("")}</tbody></table>`;
           return `
                 <div class="relative z-10 w-60 rounded-lg border-2 font-roboto bg-slateus-700 border-slateus-400">
-                    <div class="tt-root">${header}${table}</div>
+                    <div class="tt-root w-full">${header}${table}</div>
                 </div>`;
         },
       },
